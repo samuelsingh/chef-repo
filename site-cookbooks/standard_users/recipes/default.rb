7bd24d06d40d4404a02636c8d65dc458ca898ea6
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-03-27 18:13:29 +0000

    Added standard_users recipe
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1165 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ site-cookbooks/standard_users/recipes/default.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: standard_users
+# Recipe:: default
+#
+# Copyright 2010, Map of Medicine
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+group "sysadmin"  do
+  gid 10009
+end
+
+#user "sysadmin"  do
+#  comment "Standard Sysadmin User"
+#  uid "10009"
+#  gid "sysadmin"
+#  home "/home/sysadmin"
+#  shell "/bin/bash"
+#  password "$6$EftNV8G/$Z9PfDF/ss6.FGFyJLE8oeFGXfD9g20Bf72m6D5ShhImW3NIAzXx91y3D9uCU.cvtH0N/2a/VRjIxJp7bpN2nH."
+#end
