071eb0e3b1bef39797a7625c8de773708b55e4f0
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-04-13 16:06:17 +0000

    Added zones/name metadata property
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1188 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/zones/metadata.rb
+++ site-cookbooks/zones/metadata.rb
@@ -4,3 +4,8 @@ license          "Apache 2.0"
 description      "Installs/Configures zones"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
+
+attribute "zones/name",
+  :display_name => "Zone name",
+  :description => "Name used to identify a particular hosting zone",
+  :default => "unset"
