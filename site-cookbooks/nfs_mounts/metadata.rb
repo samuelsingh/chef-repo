f3f08bfb899007daa5b29183f9007de87db5a282
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-04-13 17:15:12 +0000

    Added metadata
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1191 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/nfs_mounts/metadata.rb
+++ site-cookbooks/nfs_mounts/metadata.rb
@@ -4,3 +4,28 @@ license          "Apache 2.0"
 description      "Installs/Configures nfs_mounts"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
+
+attribute "nfs_mounts",
+  :display_name => "nfs_mounts",
+  :description => "Hash of NFS mount points",
+  :type => "hash"
+
+attribute "nfs_mounts/name",
+  :display_name => "mount point",
+  :description => "Name of mount point",
+  :type => "hash"
+
+attribute "nfs_mounts/name/host",
+  :display_name => "host",
+  :description => "NFS host",
+  :default => ""
+
+attribute "nfs_mounts/name/hostdir",
+  :display_name => "host directory",
+  :description => "NFS mount point host directory",
+  :default => ""
+
+attribute "nfs_mounts/name/clientdir",
+  :display_name => "client directory",
+  :description => "NFS mount point client directory",
+  :default => ""
