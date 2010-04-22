dd8c969d108d21652b71d6e4ad6b7146f451c6c1
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-04-22 16:12:33 +0000

    Adding hosts file recipe
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1230 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/euaws-common.rb
+++ roles/euaws-common.rb
@@ -2,7 +2,7 @@ name "euaws-common"
 description "Configuration common to all hosts"
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 # run_list "recipe[zones]", "recipe[nfs_mounts]"
-run_list "recipe[zones]"
+run_list "recipe[zones], recipe[hosts]"
 
 # Attributes applied if the node doesn't have it set already.
 #default_attributes()
