4f26123cc57ee0940f9476fd39f437620f8698d3
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-04-28 16:45:16 +0000

    Updating web server reload
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1260 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/apache2/recipes/default.rb
+++ site-cookbooks/apache2/recipes/default.rb
@@ -38,6 +38,8 @@ service "apache2" do
     reload_command "/sbin/service httpd reload && sleep 1"
   when "debian","ubuntu"
     service_name "apache2"
+    restart_command "/sbin/service httpd restart && sleep 1"
+    reload_command "/sbin/service httpd reload && sleep 1"
   end
   supports value_for_platform(
     "debian" => { "4.0" => [ :restart, :reload ], "default" => [ :restart, :reload, :status ] },
