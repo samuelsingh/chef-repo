673be6f2da6154b6f1e3b82ff3f45fb236f3d1ce
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-05-17 20:30:00 +0000

    Adding mom-scripts
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1317 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ site-cookbooks/mom-scripts/attributes/mom-scripts.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: mom-scripts
+# Attributes:: mom-scripts
+#
+# Copyright 2010, Map of Medicine
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set_unless[:mom-scripts][:svn_user] = "chef"
+set_unless[:mom-scripts][:svn_pass] = "aiVahx7s"
---
+++ site-cookbooks/mom-scripts/recipes/default.rb
@@ -0,0 +1,53 @@
+#
+# Cookbook Name:: mom-scripts
+# Recipe:: default
+#
+# Copyright 2010, Map of Medicine
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+subversion "Standard /usr" do
+  repository "http://svn.mapofmedicine.com/map-sys/branches/INFRASTRUCTURE_3.0/standard/ALL/usr"
+  revision "HEAD"
+  destination "/usr"
+  svn_username "#{node[:mom-scripts][:svn_user]}"
+  svn_password "#{node[:mom-scripts][:svn_pass]}"
+  action :export
+end
+
+subversion "Standard /etc" do
+  repository "http://svn.mapofmedicine.com/map-sys/branches/INFRASTRUCTURE_3.0/standard/ALL/etc"
+  revision "HEAD"
+  destination "/etc"
+  svn_username "#{node[:mom-scripts][:svn_user]}"
+  svn_password "#{node[:mom-scripts][:svn_pass]}"
+  action :export
+end
+
+directory "/etc/map-fabric" do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+  not_if "test -d /etc/map-fabric"
+end
+
+template "zone-id" do
+  path "/etc/map-fabric/zone-id"
+  source "zone-id.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  backup false
+end
