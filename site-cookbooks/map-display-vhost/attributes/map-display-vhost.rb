107a04ba3d4e7c49ee7a36a209be167dce0c4650
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-05-20 21:18:03 +0000

    map-display-vhost bugfix
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1345 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/map-display-vhost/attributes/map-display-vhost.rb
+++ site-cookbooks/map-display-vhost/attributes/map-display-vhost.rb
@@ -18,7 +18,7 @@
 #
 
 set_unless[:map_display_vhost][:hostname] = "app.map-cloud-01.eu"
-set_unless[:map_display_vhost][:aliases] = "[]"
+set_unless[:map_display_vhost][:aliases] = []
 set_unless[:map_display_vhost][:deploy_dir] = "/var/shared/deployment/app"
 set_unless[:map_display_vhost][:appserver] = "md-app-01.map-cloud-01.eu"
 set_unless[:map_display_vhost][:tomcat_mgr_ips] = []
