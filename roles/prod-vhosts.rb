4ea920ee51896ffab8d999e371d7e193f4e481a5
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-05-28 10:41:24 +0000

    Pushing mms-vhosts changes
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1401 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/prod-vhosts.rb
@@ -0,0 +1,18 @@
+name "prod-vhosts"
+description "Configures apache vhosts for production use"
+# List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
+# run_list "recipe[zones]", "recipe[nfs_mounts]"
+run_list "recipe[mms-vhosts]"
+
+override_attributes(
+  "mms-vhosts" => {
+    "mms.map-cloud-01.eu" => {
+      "srv_aliases" => [],
+      "deploy_dir" => "/var/shared/deployment/root-mms",
+      "appserver" => "rootmms-test-01.map-cloud-01.eu",
+      "restricted_ips" => [],
+      "holding_page" => "false",
+      "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause."
+    }
+  }
+)
