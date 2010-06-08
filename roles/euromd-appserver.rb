de7167fbb5362995aa692373380c1b70c879d68f
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-06-08 11:46:12 +0000

    Fixed role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1418 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/euromd-appserver.rb
+++ roles/euromd-appserver.rb
@@ -2,7 +2,7 @@ name "euromd-appserver"
 description "Configures Live MD application server"
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 # run_list "recipe[zones]", "recipe[nfs_mounts]"
-run_list "recipe[java]", "recipe[tomcat]", "recipe[md-appserver]"
+run_list "recipe[java]", "recipe[tomcat]", "recipe[map-display]"
 
 override_attributes(
   "map_display" => {
