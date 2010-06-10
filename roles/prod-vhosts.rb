739c2c24a915427579180638b5b61a56a850263a
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-06-10 11:39:36 +0000

    Added Client MMS to vhosts
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1437 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/prod-vhosts.rb
+++ roles/prod-vhosts.rb
@@ -14,6 +14,15 @@ override_attributes(
       "holding_page" => "false",
       "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause.",
       "lb_alive_port" => 0
+    },
+    "localise.map-cloud-01.eu" => {
+      "srv_aliases" => [],
+      "deploy_dir" => "/var/shared/deployment/client-mms",
+      "appserver" => "clientmms-app-01.map-cloud-01.eu",
+      "restricted_ips" => [],
+      "holding_page" => "false",
+      "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause.",
+      "lb_alive_port" => 0
     }
   }
 )
