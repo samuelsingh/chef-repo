1da05d6bcb5ab33d7f1c0e2ac8ddbc59fc91f430
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-06-29 20:07:35 +0000

    Adding generic-tomcat-vhost
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1522 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ site-cookbooks/generic-tomcat-vhost/attributes/generic-tomcat-vhost.rb
@@ -0,0 +1,34 @@
+#
+# Cookbook Name:: generic-tomcat-vhost
+# Attributes:: mms-vhost
+#
+# Copyright 2010, Map of Medicine
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set_unless[:generic_tomcat_vhost] = {
+  "unset.mapofmedicine.com" => {
+    "srv_aliases" => [],
+    "webapps" => {
+      "webapp" => {
+        "port" => "9000",
+        "appserver" => "unset-app-01.map-cloud-01.eu",
+        "primary" => "true"
+      }
+    },
+    "restricted_ips" => [],
+    "holding_page" => "false",
+    "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause."
+  }
+}
