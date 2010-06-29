9065adf074b97d35425bb4587b1ac27e2ff57116
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-06-29 15:18:00 +0000

    Adding map-cloud-01-common role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1516 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/map-cloud-01-common.rb
@@ -0,0 +1,11 @@
+name "map-cloud-01-eu-common"
+description "Configuration common to all machines in map-cloud-01.eu"
+# List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
+# run_list "recipe[zones]", "recipe[nfs_mounts]"
+run_list "recipe[zones]", "recipe[nfs_mounts]", "recipe[standard_users]", "recipe[sudo]", "recipe[openssh-server-ec2]", "recipe[exim::satellite]", "recipe[extra-repos]", "recipe[mom-scripts]", "recipe[ntp]"
+
+override_attributes(
+  "zones" => {
+    "name" =>  "euaws"
+  }
+)
