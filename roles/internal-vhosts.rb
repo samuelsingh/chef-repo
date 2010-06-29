1fdb260664af1c98598ca8f281a80b4be8e7af02
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-06-29 20:24:19 +0000

    Adding generic-tomcat-vhost
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1523 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/internal-vhosts.rb
@@ -0,0 +1,36 @@
+name "internal-vhosts"
+description "Configures apache vhosts for internal production use"
+# List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
+# run_list "recipe[zones]", "recipe[nfs_mounts]"
+run_list "recipe[generic-tomcat-vhost]"
+
+override_attributes(
+  "generic_tomcat_vhost" => {
+    "knowledge.mapofmedicine.com" => {
+      "srv_aliases" => ["knowledge.map-cloud-01.eu"],
+      "webapps" => {
+        "confluence" => {
+          "port" => "9001",
+          "appserver" => "jirawiki-app-01.map-cloud-01.eu",
+          "primary" => "true"
+        }
+      },
+      "restricted_ips" => [],
+      "holding_page" => "false",
+      "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause."
+    },
+    "tracking.mapofmedicine.com" => {
+      "srv_aliases" => ["tracking.map-cloud-01.eu"],
+      "webapps" => {
+        "jira" => {
+          "port" => "9002",
+          "appserver" => "jirawiki-app-01.map-cloud-01.eu",
+          "primary" => "true"
+        }
+      },
+      "restricted_ips" => [],
+      "holding_page" => "false",
+      "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause."
+    }
+  }
+)
