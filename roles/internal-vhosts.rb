d944ed0dcb5dc8e09c0c11c0b291a6599090ba8f
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-06-29 20:56:51 +0000

    Bugfix for generic-tomcat-vhost
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1529 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/internal-vhosts.rb
+++ roles/internal-vhosts.rb
@@ -11,10 +11,10 @@ override_attributes(
       "webapps" => {
         "confluence" => {
           "port" => "9001",
-          "appserver" => "jirawiki-app-01.map-cloud-01.eu",
-          "primary" => "true"
+          "appserver" => "jirawiki-app-01.map-cloud-01.eu"
         }
       },
+      "primary_webapp" => "confluence",
       "restricted_ips" => [],
       "holding_page" => "false",
       "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause."
@@ -24,10 +24,10 @@ override_attributes(
       "webapps" => {
         "jira" => {
           "port" => "9002",
-          "appserver" => "jirawiki-app-01.map-cloud-01.eu",
-          "primary" => "true"
+          "appserver" => "jirawiki-app-01.map-cloud-01.eu"
         }
       },
+      "primary_webapp" => "jira",
       "restricted_ips" => [],
       "holding_page" => "false",
       "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause."
