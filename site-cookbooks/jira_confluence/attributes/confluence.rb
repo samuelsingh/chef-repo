6152f8e162201d01cf9c3367012fa36d78e7415d
jasong <jasong@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-07-02 13:36:02 +0000

    basic confluence recipe
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1550 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ site-cookbooks/jira_confluence/attributes/confluence.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: confluence 
+# Attributes:: confluence 
+#
+
+# type-version-ear/war
+set_unless[:confluence][:version]           = "confluence-3.2.1_01"
+set_unless[:confluence][:install_path]      = "/var/tomcat/server9001"
+set_unless[:confluence][:run_user]          = "tomcat"
+set_unless[:confluence][:database]          = "mysql"
+set_unless[:confluence][:database_host]     = "localhost"
+set_unless[:confluence][:database_user]     = "confluence"
+set_unless[:confluence][:database_password] = "ecneulfnoc"
