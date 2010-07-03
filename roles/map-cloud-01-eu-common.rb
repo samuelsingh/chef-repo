79d390d5ea393c3d53a861a46148688edbadf4d8
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-07-03 15:17:22 +0000

    Adding snmpd to the map-cloud-01-eu-common role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1561 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/map-cloud-01-eu-common.rb
+++ roles/map-cloud-01-eu-common.rb
@@ -2,10 +2,15 @@ name "map-cloud-01-eu-common"
 description "Configuration common to all machines in map-cloud-01.eu"
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 # run_list "recipe[zones]", "recipe[nfs_mounts]"
-run_list "recipe[zones]", "recipe[nfs_mounts]", "recipe[standard_users]", "recipe[sudo]", "recipe[openssh-server-ec2]", "recipe[exim::satellite]", "recipe[extra-repos]", "recipe[mom-scripts]", "recipe[ntp]"
+run_list "recipe[zones]", "recipe[nfs_mounts]", "recipe[standard_users]", "recipe[sudo]", "recipe[openssh-server-ec2]", "recipe[exim::satellite]", "recipe[extra-repos]", "recipe[mom-scripts]", "recipe[ntp]", "recipe[snmpd]"
 
 override_attributes(
   "zones" => {
     "name" =>  "euaws"
+  },
+  "snmpd" => {
+    "community" => "ufahji3K",
+    "syslocation" => "map-cloud-01.eu, amazon eu-west-1",
+    "syscontact" => "systems@mapofmedicine.com"
   }
 )
