80480c1fa4cb8b59800450df7b3e5127c769720d
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-07-03 14:27:10 +0000

    Adding snmpd recipe
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1555 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ site-cookbooks/snmpd/recipes/default.rb
@@ -0,0 +1,49 @@
+#
+# Cookbook Name:: snmpd
+# Recipe:: default
+#
+# Copyright 2010, Map of Medicine
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# We should make this more intelligent, so that snmp looks for the Zenoss server corresponding to its domain
+monitoring_server = node[:zenoss][:server] ? node[:zenoss][:server] : search(:node, "zenoss_server:true").map { |n| n["ipaddress"] }.first
+
+package "snmpd" do
+  case node[:platform]
+  when "debian","ubuntu"
+    package_name "snmpd"
+  end
+  action :install
+end
+
+template "/etc/snmp/snmpd.conf" do
+  source "snmpd.conf.erb"
+  mode 0600
+  owner "root"
+  group "root"
+  variables(
+    :source_ip => monitoring_server,
+    :community_string => node[:snmpd][:community],
+    :syslocation => node[:snmpd][:syslocation],
+    :syscontact => node[:snmpd][:syscontact]
+  )
+  notifies :reload, resources(:service => "snmpd")
+  only_if "test -d /etc/snmp"
+end
+
+service "snmpd"  do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
