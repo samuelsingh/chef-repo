4c20404a6ee7a3f1fde026d36331aa3675e26d13
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-07-04 12:24:18 +0000

    Adding Zenoss vhost
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1564 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/internal-vhosts.rb
+++ roles/internal-vhosts.rb
@@ -2,7 +2,7 @@ name "internal-vhosts"
 description "Configures apache vhosts for internal production use"
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 # run_list "recipe[zones]", "recipe[nfs_mounts]"
-run_list "recipe[generic-tomcat-vhost]"
+run_list "recipe[generic-tomcat-vhost]", "recipe[zenoss::vhost]"
 
 override_attributes(
   "generic_tomcat_vhost" => {
