a926c01a723beed01975af9157b93f28953a4825
jasong <jasong@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-07-06 12:33:58 +0000

    name change
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1576 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ site-cookbooks/confluence_mom/recipes/default.rb
@@ -0,0 +1,45 @@
+#
+# Cookbook Name:: Confluence 
+# Recipe:: default
+#
+# Manual Steps!
+#
+# MySQL:
+#
+#   create database confluence character set utf8;
+#   grant all privileges on confluence.* to 'confluence'@'localhost' identified by '$confluence_password';
+#   flush privileges;
+include_recipe "java"
+include_recipe "tomcat"
+
+remote_file "/etc/init.d/tomcat9001" do
+  source "tomcat9001"
+  mode 0755
+end
+
+template "#{node[:confluence][:install_path]}/conf/server.xml" do
+  source "server.xml.erb"
+  mode 0755
+end
+
+template "#{node[:confluence][:install_path]}/conf/web.xml" do
+  source "web.xml.erb"
+  mode 0755
+end
+
+template "#{node[:confluence][:install_path]}/conf/tomcat-users.xml" do
+  source "tomcat-users.xml.erb"
+  mode 0755
+end
+
+template "#{node[:confluence][:install_path]}/conf/Catalina/localhost/confluence.xml" do
+  source "confluence.xml.erb"
+  mode 0755
+end
+
+template "#{node[:confluence][:install_path]}/conf/Catalina/localhost/manager-confluence.xml" do
+  source "manager-confluence.xml.erb"
+  mode 0755
+end
+ 
+
