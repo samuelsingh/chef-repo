2337c92fdaa111acca6b27dfbbe15652eac35235
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-07-08 20:30:52 +0000

    Updating roles
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1609 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/map-cloud-01-eu-common.rb
+++ roles/map-cloud-01-eu-common.rb
@@ -2,7 +2,7 @@ name "map-cloud-01-eu-common"
 description "Configuration common to all machines in map-cloud-01.eu"
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 # run_list "recipe[zones]", "recipe[nfs_mounts]"
-run_list "recipe[zones]", "recipe[nfs_mounts]", "recipe[standard_users]", "recipe[sudo]", "recipe[openssh-server-ec2]", "recipe[exim::satellite]", "recipe[extra-repos]", "recipe[mom-scripts]", "recipe[ntp]", "recipe[snmpd]", "recipe[zenoss::client]"
+run_list "recipe[zones]", "recipe[nfs_mounts]", "recipe[standard_users]", "recipe[sudo]", "recipe[openssh-server-ec2]", "recipe[exim::satellite]", "recipe[extra-repos]", "recipe[mom-scripts]", "recipe[ntp]", "recipe[snmpd]", "recipe[zenoss::client]", "recipe[s3sync]"
 
 override_attributes(
   "zones" => {
@@ -12,5 +12,12 @@ override_attributes(
     "community" => "ufahji3K",
     "syslocation" => "map-cloud-01.eu, amazon eu-west-1",
     "syscontact" => "systems@mapofmedicine.com"
+  }
+  "s3sync" => {
+    "aws_calling_format" => "SUBDOMAIN" # Required for EU buckets
+  },
+  "ec2" => {
+    "ec2_access_key" => "",
+    "ec2_secret_key" => ""
   }
 )
