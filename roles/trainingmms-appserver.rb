79dc49b516a1651e6830b66ae06a0b8e981b5b40
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-07-14 14:27:15 +0000

    Adding trainingmms-appserver role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1636 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/trainingmms-appserver.rb
@@ -0,0 +1,41 @@
+name "trainingmms-appserver"
+description "Configures Client MMS application server"
+# List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
+# run_list "recipe[zones]", "recipe[nfs_mounts]"
+run_list "recipe[java]", "recipe[tomcat]", "recipe[mms]", "recipe[mms::cs-tools]", "recipe[mms::queue-manager]"
+default_attributes(
+  "mms" => {
+    "dbhost" => "lightmms-db-01.map-cloud-01.eu"
+  }
+)
+override_attributes(
+  "mms" => {
+    "fqdn" =>  "training-mms.map-cloud-01.eu",
+    "deploy_dir" =>  "/var/shared/deployment/training-mms",
+    "deployment_name" => "Training MMS",
+    "contentpath" => "/var/mms/content-out",
+    "content_in" => "/var/mms/content-in",
+    "deployment" => {
+      "id" => "50",
+      "external_start" => "30000001",
+      "external_end" => "40000000"
+    },
+    "quartz" => {
+      "user" => "support@mapofmedicine.com",
+      "password" => "Ogohgha8"
+    },
+    "mapmanager" => {
+      "dbname" => "training_mcs"
+    },
+    "repository" => {
+      "dbname" => "training_repo"
+    },
+    "mom" => {
+      "dbname" => "training_preview"
+    },
+    "athens_link" => "true",
+    "multiple_views" => "true",
+    "dbuser" => "mtmuser",
+    "dbpass" => "MtMUs3r"
+  }
+)
