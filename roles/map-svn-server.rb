d5578ba995e8080ed39542fd7a6ec63b6326a63d
kief <kief@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-07-19 14:43:44 +0000

    PROJ-75 Added mod_dav_svn module to svn server role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1675 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/map-svn-server.rb
+++ roles/map-svn-server.rb
@@ -2,7 +2,7 @@ name "map-svn-server"
 description "Map of Medicine Subversion server"
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 # run_list "recipe[zones]", "recipe[nfs_mounts]"
-run_list "recipe[subversion], recipe[apache2]", "recipe[apache2::mod_rewrite]", "recipe[apache2::mod_headers]", "recipe[apache2::mod_dav]", "recipe[apache2::mod_authnz_ldap]", "recipe[apache2::mod_proxy_connect]", "recipe[apache2::mod_ldap]", "recipe[apache2::mod_proxy_http]", "recipe[apache2::mod_status]", "recipe[apache2::mod_cgi]", "recipe[apache2::mod_log_config]", "recipe[apache2::mod_expires]", "recipe[apache2::mod_include]", "recipe[apache2::mod_log_forensic]", "recipe[apache2::mod_info]"
+run_list "recipe[subversion], recipe[apache2]", "recipe[apache2::mod_rewrite]", "recipe[apache2::mod_headers]", "recipe[apache2::mod_dav]", "recipe[apache2::mod_dav_svn]", "recipe[apache2::mod_authnz_ldap]", "recipe[apache2::mod_proxy_connect]", "recipe[apache2::mod_ldap]", "recipe[apache2::mod_proxy_http]", "recipe[apache2::mod_status]", "recipe[apache2::mod_cgi]", "recipe[apache2::mod_log_config]", "recipe[apache2::mod_expires]", "recipe[apache2::mod_include]", "recipe[apache2::mod_log_forensic]", "recipe[apache2::mod_info]"
 
 override_attributes(
   "apache" => {
