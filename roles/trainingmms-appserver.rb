129354556dfe6f49aec43c5016d1559471687c26
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-07-27 11:17:04 +0000

    Updating Training MMS version number
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1714 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/trainingmms-appserver.rb
+++ roles/trainingmms-appserver.rb
@@ -15,7 +15,7 @@ override_attributes(
     "deployment_name" => "Training MMS",
     "contentpath" => "/var/mms/content-out",
     "content_in" => "/var/mms/content-in",
-    "version" => "2.6.4.3.ALL.4.36087",
+    "version" => "2.6.4.3.ALL.6.36227",
     "deployment" => {
       "id" => "50",
       "external_start" => "30000001",
