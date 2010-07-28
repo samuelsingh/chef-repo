179c42e7af6a51aaf8ae607dc4d370cbb5ba808c
jasong <jasong@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-07-28 17:44:13 +0000

    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1729 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/hgmde/attributes/hgmde.rb
+++ site-cookbooks/hgmde/attributes/hgmde.rb
@@ -4,7 +4,10 @@
 #
 
 # war
-set_unless[:hgmde][:version]           = "healthguides-20100316"
-set_unless[:hgmde][:install_path]      = "/var/tomcat/server9001"
+set_unless[:hgmde][:hg_version]           = "healthguides-20100316"
+set_unless[:hgmde][:hg_install_path]      = "/var/tomcat/server9001"
 set_unless[:hgmde][:run_user]          = "tomcat"
-set_unless[:hgmde][:war_path]          = ""
+set_unless[:hgmde][:hg_war_path]          = "/var"
+set_unless[:hgmde][:mde_version]           = "evidence-20100623"
+set_unless[:hgmde][:mde_install_path]      = "/var/tomcat/server9002"
+set_unless[:hgmde][:mde_war_path]          = "/var"
