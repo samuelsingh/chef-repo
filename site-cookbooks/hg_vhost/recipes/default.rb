8defe7fbdd29ab755a1059a4ffe8ddad8444b214
jasong <jasong@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-07-28 17:22:14 +0000

    directory amendment
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1728 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/hg_vhost/recipes/default.rb
+++ site-cookbooks/hg_vhost/recipes/default.rb
@@ -22,7 +22,7 @@ node[:generic_tomcat_vhost].each do |hostname,params|
   if defined?(node[:apache][:dir])
     
     template "#{node[:apache][:dir]}/sites-available/#{hostname}.conf" do
-      source "generic-tomcat-vhost.conf.erb"
+      source "#{hostname}.conf.erb"
       mode 0644
       owner "sysadmin"
       group "sysadmin"
