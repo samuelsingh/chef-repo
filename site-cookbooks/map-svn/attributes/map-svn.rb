1677cbaf0a9742119733d0ae23a2fa1bf800fdae
kief <kief@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-07-28 10:26:29 +0000

    PROJ-75 Implementing the building of vhost sections for each svn repository.
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1716 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/map-svn/attributes/map-svn.rb
+++ site-cookbooks/map-svn/attributes/map-svn.rb
@@ -9,7 +9,23 @@ if recipe?("map_svn::vhost")
   set_unless[:map_svn][:vhost][:hostname] = "unset.unset.com"
   set_unless[:map_svn][:vhost][:srv_aliases] = []
   set_unless[:map_svn][:svn_root] = "/var/svn"
-  set_unless[:map_svn][:svn_repos] = []
+  set_unless[:map_svn][:svn_repos] = [
+  	{
+  		:name => "map-test",
+  		:auth_name => "X",
+  		:ldap_group => "cn=mom-ias,ou=groups,dc=mapofmedicine,dc=com"
+  	},
+  	{
+  		:name => "map-dev",
+  		:auth_name => "Map Development Source",
+  		:ldap_group => "cn=map-dev-committers,ou=groups,dc=mapofmedicine,dc=com"
+  	},
+  	{
+  		:name => "map-sys",
+  		:auth_name => "Map Infrastructure Configuration",
+  		:ldap_group => "cn=mom-ias,ou=groups,dc=mapofmedicine,dc=com"
+  	}
+  ]
   set_unless[:map_svn][:ldap][:server] = "unset.unset.com"
   set_unless[:map_svn][:ldap][:port] = "389"
   set_unless[:map_svn][:ldap][:url_path] = "unset"
