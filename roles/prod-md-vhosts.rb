2c73ff49c49b26063dc78576c35820dce633bc84
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-08-02 10:43:12 +0000

    Added prod-md-vhosts role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1753 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/prod-md-vhosts.rb
@@ -0,0 +1,18 @@
+name "prod-md-vhosts"
+description "Configures apache vhosts using a dedicated ELB for production use"
+# List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
+# run_list "recipe[zones]", "recipe[nfs_mounts]"
+run_list "recipe[map-display-vhost]"
+
+override_attributes(
+  "map_display_vhost" => {
+    "euro-md.map-cloud-01.eu" => {
+      "srv_aliases" => [],
+      "deploy_dir" => "/var/shared/deployment/euro-md",
+      "appserver" => "euromd-app-01.map-cloud-01.eu",
+      "holding_page" => "false",
+      "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause.",
+      "lb_alive_port" => 8050
+    }
+  }
+)
