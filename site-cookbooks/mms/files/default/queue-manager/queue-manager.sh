cf973e83347db5c8c7b84eab42279b3eae1b30c4
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-08-25 14:59:39 +0000

    Added fix to batch processing tool help behaviour
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1826 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/mms/files/default/queue-manager/queue-manager.sh
+++ site-cookbooks/mms/files/default/queue-manager/queue-manager.sh
@@ -10,7 +10,16 @@ for JAR in $DIR/lib/*.jar; do
     CLASSPATH="$CONFIG:$HLP:$CLASSPATH:$JAR"
 done
 
-COMMAND="java -Xms512M -Xmx512M -cp $CLASSPATH com.oyster.mom.contentserver.batch.BatchTools $@"
+# Fix to make the -h / --help switch behave correctly.
+# Currently, help is only triggered when no parameters are given
+# Logged as IST-7142
+
+if [ "$1" = "--help" ] || [ "$1" = "-h" ] ; then
+    COMMAND="java -Xms512M -Xmx512M -cp $CLASSPATH com.oyster.mom.contentserver.batch.BatchTools"
+else
+    COMMAND="java -Xms512M -Xmx512M -cp $CLASSPATH com.oyster.mom.contentserver.batch.BatchTools $@"
+fi
+
 
 if [ $(id -u) = "0" ] ; then
     su $USER -c "$COMMAND"
