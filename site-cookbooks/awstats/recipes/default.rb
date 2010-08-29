e5e5af27446f2f1e42618aa83f7cf2fdef4c2b5a
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-08-29 20:23:16 +0000

    Fixing cron.d snippets
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1853 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/awstats/recipes/default.rb
+++ site-cookbooks/awstats/recipes/default.rb
@@ -33,6 +33,8 @@ end
 
 node[:awstats].each do |vhost,params|
   
+  nodotvhost = vhost.gsub!('.','-')
+  
   template "/etc/awstats/awstats.#{vhost}.conf" do
     source "awstats.conf.template.erb"
     mode 0644
@@ -46,7 +48,7 @@ node[:awstats].each do |vhost,params|
     only_if "test -d /etc/awstats"
   end
 
-  template "/etc/cron.d/awstats.#{vhost}" do
+  template "/etc/cron.d/awstats-#{nodotvhost}" do
     source "awstats-cron-template.erb"
     mode 0644
     owner "root"
