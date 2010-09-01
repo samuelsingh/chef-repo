5893c3ec58e8adbd455726a974a4360318bc5aa4
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-09-01 20:48:09 +0000

    Bugfix
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1884 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/hg-mde-vhost/recipes/default.rb
+++ site-cookbooks/hg-mde-vhost/recipes/default.rb
@@ -28,7 +28,7 @@ node[:hg_mde_vhost].each do |hostname,params|
     # Figures out whether to link to AWStats
     awstats_srv = search(:node, "awstats:#{hostname}").map { |n| n["fqdn"] }.first
     
-    unless awstats_srv.empty?
+    unless empty?(awstats_srv)
       awstats = false
     else
       awstats = true
