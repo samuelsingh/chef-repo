cd1f66dcffc91339bfe4de9418186f3f575fe172
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-09-01 17:29:23 +0000

    Refactor of HG / MDE vhost configuration
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1872 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/hgpe-vhosts.rb
@@ -0,0 +1,35 @@
+name "hgpe-vhosts"
+description "Configures HG / MDE vhosts"
+
+override_attributes(
+  "hg_mde_vhost" => {
+    "healthguides.mapofmedicine.com" => {
+      "srv_aliases" => ["healthguides.map-cloud-01.eu"],
+      "webapps" => {
+        "choices" => {
+          "port" => "9001",
+          "appserver" => "127.0.0.1"
+        }
+      },
+      "primary_webapp" => "choices",
+      "webapp_base" => "/var/tomcat/server9001/webapps",
+      "restricted_ips" => [],
+      "holding_page" => "false",
+      "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause."
+    },
+    "eng.mapofmedicine.com" => {
+      "srv_aliases" => ["eng.map-cloud-01.eu"],
+      "webapps" => {
+        "evidence" => {
+          "port" => "9002",
+          "appserver" => "127.0.0.1"
+        }
+      },
+      "primary_webapp" => "evidence",
+      "webapp_base" => "/var/tomcat/server9002/webapps",
+      "restricted_ips" => [],
+      "holding_page" => "false",
+      "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause."
+    }
+  }
+)
