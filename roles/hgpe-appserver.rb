fbb7633275790720dbd99b573f12ace00c497bc2
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-09-01 13:44:59 +0000

    Bugfix
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1867 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/hgpe-appserver.rb
+++ roles/hgpe-appserver.rb
@@ -1,7 +1,7 @@
 name "hgpe-appserver"
 description "Configures HG / MDE application server"
 
-run_list "recipe[java]", "recipe[tomcat]", "recipe[hg-mde]
+run_list "recipe[java]", "recipe[tomcat]", "recipe[hg-mde]"
 
 override_attributes(
   "hg_mde" => {
