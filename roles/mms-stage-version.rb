ebe6901518ec33ef9ae2f8045141b4682b1a00c6
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-09-07 11:03:37 +0000

    Adding role for MMS staging
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1917 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/mms-stage-version.rb
@@ -0,0 +1,10 @@
+name "mms-stage-version"
+description "MMS staging version snippet"
+
+override_attributes(
+  "mms" => {
+    "version" => "2.6.5.ALL.1.36493",
+    "me_version" => "7.6.9",
+    "dict_version" => "0.2"
+  }
+)
