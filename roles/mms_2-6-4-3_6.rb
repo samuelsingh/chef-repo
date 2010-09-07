2100984358dc6636d44b0dddeee68753ab94f642
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-09-07 10:53:54 +0000

    Adding ME and Dict versions to MMS role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1916 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/mms_2-6-4-3_6.rb
+++ roles/mms_2-6-4-3_6.rb
@@ -3,6 +3,8 @@ description "MMS 2.6.4.3-6 version snippet"
 
 override_attributes(
   "mms" => {
-    "version" => "2.6.4.3.ALL.6.36227"
+    "version" => "2.6.4.3.ALL.6.36227",
+    "me_version" => "7.6.8",
+    "dict_version" => "0.1"
   }
 )
