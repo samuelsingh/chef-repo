eea5d78de694af0f82d714e8e631c838c967adf6
kief <kief@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-09-09 13:18:28 +0000

    PROJ-75 Added the svn-01.mapofmedicine.com hostname to the vhost
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1925 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/map-svn-server.rb
+++ roles/map-svn-server.rb
@@ -37,7 +37,7 @@ override_attributes(
   :map_svn => {
     :vhost => {
       :hostname => "svn-01.map-cloud-01.eu",
-      :srv_aliases => [ "svn.mapofmedicine.com", "svn-01", "svn-test.mapofmedicine.com" ]
+      :srv_aliases => [ "svn-01.mapofmedicine.com", "svn-test.mapofmedicine.com" ]
     },
     :ldap => {
       :server => "ldap01.map-cloud-01.eu",
