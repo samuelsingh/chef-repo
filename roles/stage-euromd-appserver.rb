42d8b1b1bcf1502c932e6182f0acf31e4814f8d4
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-09-10 15:12:56 +0000

    Trying to make the default attributes in the role work
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1927 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/stage-euromd-appserver.rb
+++ roles/stage-euromd-appserver.rb
@@ -3,18 +3,5 @@ description "Configures Stage MD application server"
 
 run_list "recipe[java]", "recipe[tomcat]", "recipe[map-display]", "recipe[map-display::contentloader]"
 
-override_attributes(
-  "map_display" => {
-    "deploy_dir" =>  "/var/shared/deployment/stage/euro-md",
-    "dbhost" => "stage-db-02.map-cloud-01.eu",
-    "dbuser" => "mtmuser",
-    "dbpass" => "medic1"
-  }
-)
-
-default_attributes(
-  "map_display" => {
-    "md_fqdn" =>  "stage-euromd.map-cloud-01.eu",
-    "dbname" => "live_mtmdb"
-  }
-)
+default_attributes "map_display" => {"md_fqdn" =>  "stage-euromd.map-cloud-01.eu","dbname" => "live_mtmdb"}
+override_attributes "map_display" => {"deploy_dir" =>  "/var/shared/deployment/stage/euro-md","dbhost" => "stage-db-02.map-cloud-01.eu","dbuser" => "mtmuser","dbpass" => "medic1"}
