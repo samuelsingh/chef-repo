c49ce256feeef03a38eda1680ed0a87e5522654f
kief <kief@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-09-14 10:52:37 +0000

    PROJ-75 Fix the svn_backup script. The hotcopy command refuses to attempt to overwrite an existing hotcopy. The svnadmin dump command has incremental backup, which could be attractive, but this just creates a series of dumpfiles, which would all have to be restored. The svn manual doesn't recommend it. Also, I don't think that one copies the hooks and such.
    So this solution is just to rotate the hotcopy directory. Only one active copy is retained, the previous one is moved out of the way unitl the new one is completed, then it's removed.
    
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1961 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/map-svn/files/default/svn_backup.sh
+++ site-cookbooks/map-svn/files/default/svn_backup.sh
@@ -15,13 +15,26 @@ fail()
 }
 
 [ -d "$SVN_BASE" ] || fail "Missing '$SVN_BASE'"
+
+
 [ -d "$DUMP_FOLDER" ] || mkdir -p "$DUMP_FOLDER"
 [ -d "$DUMP_FOLDER" ] || fail "Can't create '$DUMP_FOLDER'"
 
 cd "$SVN_BASE" || fail "Can't cd to '$SVN_BASE'"
 for d in * ; do
   if [ -d "$SVN_BASE/$d/hooks" ] ; then
-    svnadmin hotcopy "$SVN_BASE/$d" "$DUMP_FOLDER/$d-backup" || fail "Dump of svn repository $d failed"
+    BAK0="$DUMP_FOLDER/$d.0"
+    BAK1="$DUMP_FOLDER/$d.1"
+    echo "Backing up svn repository $d"
+    if [ -d "$BAK1" ] ; then
+      rm -rf "$BAK1" || fail "Can't remove $BAK1"
+    fi
+    if [ -d "$BAK0" ] ; then
+      mv "$BAK0" "$BAK1" || fail "Can't rename $BAK0 to $BAK1"
+    fi
+    echo "Backing up $SVN_BASE/$d to $BAK0"
+    svnadmin hotcopy "$SVN_BASE/$d" "$BAK0" || fail "Backup of svn repository $d failed"
+    rm -rf "$BAK1"
   fi
 done
 
