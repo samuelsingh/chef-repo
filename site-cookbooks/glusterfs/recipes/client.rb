b9718cffd3cd66b2c96119815996cf6bf381a987
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-09-14 10:29:10 +0000

    First commit of the glusterfs client
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1960 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/glusterfs/recipes/client.rb
+++ site-cookbooks/glusterfs/recipes/client.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: glusterfs
-# Recipe:: default
+# Recipe:: client
 #
 # Copyright 2010, Map of Medicine
 #
@@ -16,3 +16,74 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+mounts = node[:glusterfs][:client][:mounts]
+glustersrvs = search(:node, "glusterfs_server:true").map { |n| n["fqdn"] }
+
+# Reverses the glustersrvs list depending on ip address, to add some randomness
+# to the mount order
+#
+glustersrvs = glustersrvs.reverse if node[:ipaddress].gsub(".", "").to_i.odd?
+
+package "glusterfs-client" do
+  case node[:platform]
+  when "debian","ubuntu"
+    package_name "glusterfs-client"
+  end
+  action :install
+end
+
+# Loads the fuse module if this hasn't
+# already happened
+#
+execute "modprobe fuse" do
+  command "modprobe fuse"
+  action :run
+  not_if "lsmod | grep fuse > /dev/null"
+end
+
+# Creates base mount directory
+#
+directory "/gfs" do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+  not_if "test -d /gfs"
+end
+
+unless glustersrvs.empty?
+
+  mounts.each do |mount|
+    
+    directory "/gfs/#{mount}" do
+      owner "root"
+      group "root"
+      mode "0755"
+      action :create
+      not_if "test -d /gfs/#{mount}"
+    end
+    
+    if glustersrvs[1].nil?
+      
+      mount "/gfs/#{mount}" do
+        device "#{glustersrvs[0]}:export_#{mount}"
+        fstype "glusterfs"
+        options "direct-io-mode=disable,noatime"
+        action [:mount, :enable]
+      end
+      
+    else
+      
+      mount "/gfs/#{mount}" do
+        device "#{glustersrvs[0]}:export_#{mount}"
+        fstype "glusterfs"
+        options "backupvolfile-server=#{glustersrvs[1]},direct-io-mode=disable,noatime"
+        action [:mount, :enable]
+      end
+      
+    end
+  
+  end
+
+end
