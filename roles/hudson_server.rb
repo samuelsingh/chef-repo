aa7f998601d3ab3246f708b746c9d81e54127dd1
kief <kief@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-09-15 11:25:29 +0000

    PROJ-76 Rudimentary role definition for the Hudson server
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1978 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/hudson_server.rb
@@ -0,0 +1,14 @@
+name "hudson_server"
+description "Map of Medicine Hudson deployment server"
+
+# List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
+# run_list "recipe[zones]", "recipe[nfs_mounts]"
+run_list 
+	"recipe[java]", "recipe[tomcat]", "recipe[hudson]"
+
+override_attributes(
+
+  :hudson => {
+	:server => {
+		:home = "/var/hudson"
+)
