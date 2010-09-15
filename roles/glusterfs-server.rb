3ff83840b8a4b71711a8d76e9d4d40c76acf5fff
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-09-15 12:20:18 +0000

    Adding extra stuff to the glusterfs role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1984 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/glusterfs-server.rb
+++ roles/glusterfs-server.rb
@@ -1,7 +1,7 @@
 name "glusterfs-server"
 description "Configures glusterfs storage node"
 
-run_list "recipe[glusterfs::ebs]","recipe[glusterfs::ephemeral]","recipe[glusterfs::server]"
+run_list "recipe[aws]","recipe[glusterfs::ebs]","recipe[glusterfs::ephemeral]","recipe[glusterfs::server]"
 
 override_attributes(
   "glusterfs" => {
