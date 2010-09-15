4a1144d9b5b8b911555834c15b10c0ec541d4b28
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-09-15 11:56:13 +0000

    Adding recipes
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@1979 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ site-cookbooks/glusterfs/recipes/ebs.rb
@@ -0,0 +1,66 @@
+#
+# Cookbook Name:: glusterfs
+# Recipe:: client
+#
+# Copyright 2010, Map of Medicine
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#"glusterfs" => {
+#  "server_volumes" => {
+#    "volume_name" => {
+#      "host" => "host_fqdn",
+#      "device" => "device_name",
+#      "mount" => "mount_point",
+#      "fstype" => "fstype"
+#    }
+#  }
+#}
+
+volumes = node[:glusterfs][:server_ebs_volumes]
+
+volumes.each do |name, conf|
+  
+  if node[:fqdn] == conf.fetch("host")
+  
+    aws_ebs_volume "#{name}" do
+      provider "aws_ebs_volume"
+      aws_access_key node[:aws][:aws_access_key]
+      aws_secret_access_key node[:aws][:aws_secret_access_key]
+      volume_id name
+      availability_zone node[:ec2][:availability_zone]
+      device conf.fetch("device")
+      action :attach
+      not_if "test -b #{conf.fetch("device")}"
+    end
+    
+    directory "#{conf.fetch("mount")}" do
+      owner "root"
+      group "root"
+      mode "0755"
+      action :create
+    end
+  
+    mount "#{conf.fetch("mount")}" do
+      device "#{conf.fetch("device")}"
+      fstype "#{conf.fetch("fstype")}"
+    end
+  
+  end
+  
+end
+
+
+
+
---
+++ site-cookbooks/glusterfs/recipes/ephemeral.rb
@@ -0,0 +1,50 @@
+#
+# Cookbook Name:: glusterfs
+# Recipe:: client
+#
+# Copyright 2010, Map of Medicine
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#"glusterfs" => {
+#  "server_eph_volumes" => {
+#    "volume_name" => {
+#      "device" => "device_name",
+#      "mount" => "mount_point",
+#      "fstype" => "fstype"
+#    }
+#  }
+#}
+
+volumes = node[:glusterfs][:server_eph_volumes]
+
+volumes.each do |name, conf|
+  
+  directory "#{conf.fetch("mount")}" do
+    owner "root"
+    group "root"
+    mode "0755"
+    action :create
+  end
+  
+  mount "#{conf.fetch("mount")}" do
+    device "#{conf.fetch("device")}"
+    fstype "#{conf.fetch("fstype")}"
+  end
+  
+end
+
+
+
+
