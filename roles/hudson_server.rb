8676fa9c599b7b5f9f8f4569d248bd2370202eb9
kief <kief@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-09-20 13:56:16 +0000

    PROJ-76 Changing the hudson_server role in a random attempt to make it actually run
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2023 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/hudson_server.rb
+++ roles/hudson_server.rb
@@ -3,8 +3,7 @@ description "Map of Medicine Hudson deployment server"
 
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 # run_list "recipe[zones]", "recipe[nfs_mounts]"
-run_list 
-	"recipe[java]", "recipe[tomcat]", "recipe[hudson]"
+run_list "recipe[java]", "recipe[tomcat]", "recipe[hudson]"
 
 override_attributes(
 
