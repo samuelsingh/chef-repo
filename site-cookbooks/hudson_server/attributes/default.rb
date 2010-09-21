3e4248d427e0d90dad0188df27b4b4603dc236e0
kief <kief@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-09-21 11:53:19 +0000

    PROJ-96 Making the default attributes for the Hudson recipe a little more strongly defined, since they appear not be overwriting values I've set previously.
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2036 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/hudson_server/attributes/default.rb
+++ site-cookbooks/hudson_server/attributes/default.rb
@@ -1,7 +1,7 @@
 
-set_unless[:hudson_server][:version]           = "hudson-current"
-set_unless[:hudson_server][:install_path]      = "/var/tomcat/server9001"
-set_unless[:hudson_server][:war_path]          = "/var/hudson/deploy-wars"
-set_unless[:hudson_server][:home]              = "/var/hudson"
-set_unless[:hudson_server][:run_user]          = "tomcat"
+set[:hudson_server][:version]           = "hudson-current"
+set[:hudson_server][:install_path]      = "/var/tomcat/server9001"
+set[:hudson_server][:war_path]          = "/var/hudson/deploy-wars"
+set[:hudson_server][:home]              = "/var/hudson"
+set[:hudson_server][:run_user]          = "tomcat"
 
