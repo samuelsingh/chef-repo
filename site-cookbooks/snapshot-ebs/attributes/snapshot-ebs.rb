519edab5e51015d21f234c4ea92b91bb31819939
kief <kief@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-09-22 08:27:58 +0000

    Hoping to get the syntax right for setting the attributes for snapshot-ebs
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2044 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/snapshot-ebs/attributes/snapshot-ebs.rb
+++ site-cookbooks/snapshot-ebs/attributes/snapshot-ebs.rb
@@ -1,15 +1,13 @@
 #
 # Cookbook Name:: snapshot_ebs
 # Attributes:: snapshot_ebs
-set :snapshot_ebs => {
-    :install_path => "/usr/local/bin",
-    :volumes => [
+set[:snapshot_ebs][:install_path] => "/usr/local/bin"
+
+set[:snapshot_ebs][:volumes] = [
 	{ :name => "mddb01",     :volume_id => "vol-fe976d97", :retention => "35" },
 	{ :name => "lightmms01", :volume_id => "vol-9a956ff3", :retention => "35" },
 	{ :name => "filer01",    :volume_id => "vol-58957f31", :retention => "35" },
 	{ :name => "svn01",      :volume_id => "vol-b844bbd1", :retention => "35" },
 	{ :name => "hudson01",   :volume_id => "vol-acfc3cc5", :retention => "35" }
     ]
-  }
-
 
