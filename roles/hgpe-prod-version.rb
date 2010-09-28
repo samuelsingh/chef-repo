ae11e787ef4e576eedb37a63aa579a313815acd4
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-09-28 12:39:18 +0000

    Updating deployed healthguides version
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2099 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/hgpe-prod-version.rb
+++ roles/hgpe-prod-version.rb
@@ -3,7 +3,7 @@ description "Specifies the HG and MDE versions currently used in production"
 
 override_attributes(
   "hg_mde" => {
-    "hg_version" =>  "choices.R12.2-Issue_3_2010-100730.183004.769.d1v1r46-August_2010-100920.093349.423.d3v17r4247_2",
-    "mde_version" =>  "evidence.R4.1-Issue_3_2010-100730.183004.769.d1v1r46-August_2010-100920.093349.423.d3v17r4247_2"
+    "hg_version" =>  "choices.R12.2-Issue_3_2010-100730.183004.769.d1v1r46-August_2010-100920.093349.423.d3v17r4247_3",
+    "mde_version" =>  "evidence.R4.1-Issue_3_2010-100730.183004.769.d1v1r46-August_2010-100920.093349.423.d3v17r4247_3"
   }
 )
