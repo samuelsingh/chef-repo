8b5e3921b2e6ab38cd5029821eac28a8edd22eb1
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-09-29 16:20:25 +0000

    Updated version of MMS deployed
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2112 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/mms-uat-version.rb
+++ roles/mms-uat-version.rb
@@ -3,7 +3,7 @@ description "MMS UAT version snippet"
 
 override_attributes(
   "mms" => {
-    "version" => "2.6.5.ALL.7.36569",
+    "version" => "2.6.5.ALL.11.36667",
     "me_version" => "7.6.9",
     "dict_version" => "0.2"
   }
