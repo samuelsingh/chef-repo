968ec5e89487bdbb786b3cc389d334b1c9e4eee6
kief <kief@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-10-04 14:04:28 +0000

    PROJ-17 Refactored the roles for QA MD deployment.
    We have the role qa-md-appserver, which is generic across any QA deployment of Map Display.
    And we have the role qa-md01-deployment, which is specific to the given deployment.
    
    So there is a single qa-md-appserver role, which can be used for multiple QA deployments of Map Display, each of which has a role qa-mdXX-deployment, which defines things specific to that deployment, such as the database connection details and vhost.
    
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2143 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/md01-qa-version.rb
+++
@@ -1,8 +0,0 @@
-name "md01-qa-version"
-description "MD version for QA deployment"
-
-override_attributes(
-  "map_display" => {
-    "version" => "2.7.0-2010-10-01_13-10-07-36704"
-  }
-)
---
+++ roles/qa-md-appserver.rb
@@ -0,0 +1,10 @@
+name "qa-md-appserver"
+description "Configures an application server for an MD QA deployment"
+
+# This is a generic role for any QA deployment of MD. Each deployment needs to
+# have its own specific role for qa-mdXX-deployment, to define things unique to
+# the deployment such as database
+
+run_list "recipe[java]", "recipe[tomcat]", "recipe[map-display]", "recipe[map-display::contentloader]", "recipe[hudson_agent]"
+
+
