2ec6ecf34c18c58349d6cbd4338e662cfdd8057b
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-10-05 19:51:01 +0000

    Updating custom PS1 variable so that it also shows correctly in the console heading
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2159 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/prod-server/files/default/custom-ps1-prefix.sh
+++ site-cookbooks/prod-server/files/default/custom-ps1-prefix.sh
@@ -1,2 +1,2 @@
 # Adds the prefix [Prod] to the console, to indicate a production system
-export PS1="\[\033[31m\][Prod] \[\033[0m\]\u@\h:\w$ "
+export PS1="\[\033[31m\][Prod] \[\033[0m\]\[\e]0;\u@\h: \w\a\]\u@\h:\w$ "
