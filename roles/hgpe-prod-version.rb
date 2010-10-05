f05979b40912f1dab90f0cedae6c64a06399166d
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-10-05 19:17:27 +0000

    Adding prod-server recipe to key machines
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2158 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/hgpe-prod-version.rb
+++ roles/hgpe-prod-version.rb
@@ -1,6 +1,8 @@
 name "hgpe-prod-version"
 description "Specifies the HG and MDE versions currently used in production"
 
+run_list "recipe[prod-server]"
+
 override_attributes(
   "hg_mde" => {
     "hg_version" =>  "choices.R12.2-Issue_3_2010-100730.183004.769.d1v1r46-August_2010-100920.093349.423.d3v17r4247_3",
