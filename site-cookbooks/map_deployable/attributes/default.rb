fb76819121b3c1011422f767b4c1302b305d5f95
kief <kief@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-10-07 10:17:25 +0000

    PROJ-17 Fleshing out the bones of a recipe that decides what changes need to be applied to an app server depending on what is currently deployed, and what package is ready to be deployed
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2163 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ site-cookbooks/map_deployable/attributes/default.rb
@@ -0,0 +1,6 @@
+
+set_unless[:fabric_deployment][:environment] = "unknown"
+
+set[:fabric_deployment][:env_package_dir] = "/var/shared/fabric-pipeline/deploy"
+
+
--- site-cookbooks/map_deployable/recipes/default.rb
+++ site-cookbooks/map_deployable/recipes/default.rb
@@ -17,8 +17,67 @@
 # limitations under the License.
 #
 
-remote_file "/usr/local/sbin/install-build" do
+cookbook_file "/usr/local/sbin/install-build" do
   source "install-build"
   mode 0755
+end
+
+environment_id = "#{node[:fabric_deployment][:environment]}"
+package_base = "#{node[:fabric_deployment][:env_package_dir]}"
+env_packages = "#{environment_id}/#{package_base}"
+hostname = "#{node[:hostname]}"
+
+current_package_file = "#{env_packages}/current-version.txt"
+deployed_here_file = "#{env_packages}/deployed-#{hostname}.txt"
+database_schema_file = "#{env_packages}/db-schema-version.txt"
+
+ruby_block "package_versions" do
+
+  # Which package is currently deployed?
+  if FileTest.exists?(deployed_here_file)
+	  deployed_package = File.open(deployed_here_file, "r").gets.first.chomp
+  else
+	  deployed_package = nil
+  end
+
+  # Which package should be deployed?
+  if FileTest.exists?(current_package_file)
+	  current_package = File.open(current_package_file, "r").gets.first.chomp
+  else
+	  current_package = nil
+  end
+
+  # Which package is the database schema set to?
+  if FileTest.exists?(database_schema_file)
+	  db_schema_version = File.open(database_schema_file, "r").gets.first.chomp
+  else
+	  db_schema_version = nil
+  end
+
+  if deployed_package == current_package && deployed_package == db_schema_version
+	# Everything is fine
+	Chef.log.debug("Package #{deployed_package} is deployed and current on #{hostname}.")
+	Chef.log.warn("Should implement something to ensure tomcat is running")
+  else
+
+	if deployed_package != current_package
+		Chef.log.info("App server #{hostname} has package #{deployed_package} deployed, needs to install #{current_package}"
+		Chef.log.warn("STOP TOMCAT")
+		Chef.log.warn("REPLACE #{deployed_package} WITH #{current_package}"
+	end
+
+	if deployed_package != db_schema_version
+		Chef.log.warn("App server #{hostname} has package #{deployed_package} deployed, database schema is for package #{db_schema_version}"
+		Chef.log.warn("STOP TOMCAT")
+	end
+
+	if current_package != db_schema_version
+		Chef.log.warn("Database needs to be upgraded or rolled back to match package #{current_package}"
+		Chef.log.warn("STOP TOMCAT")
+	end
+
+  end
+
+
 end
 
