28c126f49757e68046075dd77f42f2f573c3e624
kief <kief@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-10-08 15:23:18 +0000

    PROJ-17 minor tweak to unknown packages
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2187 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/map_deployable/attributes/default.rb
+++ site-cookbooks/map_deployable/attributes/default.rb
@@ -13,7 +13,7 @@ current_package_file = "#{env_packages}/current-version.txt"
 if FileTest.exists?(current_package_file)
 	set[:fabric_deployment][:packages][:current] = File.open(current_package_file, "r").gets.first.chomp
 else
-	set[:fabric_deployment][:packages][:current] = "_None_"
+	set[:fabric_deployment][:packages][:current] = ""
 end
 
 #
@@ -23,7 +23,7 @@ deployed_here_file = "#{env_packages}/deployed-#{node[:hostname]}.txt"
 if FileTest.exists?(deployed_here_file)
 	set[:fabric_deployment][:packages][:deployed] = File.open(deployed_here_file, "r").gets.first.chomp
 else
-	set[:fabric_deployment][:packages][:deployed] = "_None_"
+	set[:fabric_deployment][:packages][:deployed] = ""
 end
 
 #
@@ -33,7 +33,7 @@ database_schema_file = "#{env_packages}/db-schema-version.txt"
 if FileTest.exists?(database_schema_file)
 	set[:fabric_deployment][:packages][:dbschema] = File.open(database_schema_file, "r").gets.first.chomp
 else
-	set[:fabric_deployment][:packages][:dbschema] = "_None_"
+	set[:fabric_deployment][:packages][:dbschema] = ""
 end
 
 
