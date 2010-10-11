d56dd75a02888303df23250e11d46665d51a8dd4
kief <kief@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-10-11 11:21:21 +0000

    PROJ-17 Fixing syntax error
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2208 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/map_deployable/recipes/default.rb
+++ site-cookbooks/map_deployable/recipes/default.rb
@@ -54,8 +54,8 @@ ruby_block "upgrade_package" do
 
 	ajp_ports.each do |port|
 		Chef::Log.debug("Stopping Tomcat on #{port} for webapp upgrade")
-		result = `"/etc/init.d/tomcat#{port}" stop`
-		if $? != 0 do
+		result = %x{"/etc/init.d/tomcat#{port}" stop}
+		if $? != 0
 			# May have gotten an error code because tomcat was not running in the first place
 			Chef::Log.warn("Failed to stop tomcat using /etc/init.d/tomcat#{port} stop")
 			Chef::Log.warn(result)
