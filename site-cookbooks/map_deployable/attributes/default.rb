2db9e8d470b8e7ce7302d8e5680ac724107164a9
kief <kief@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-10-11 15:51:58 +0000

    PROJ-17 Changed to test file is non-zero
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2216 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/map_deployable/attributes/default.rb
+++ site-cookbooks/map_deployable/attributes/default.rb
@@ -10,7 +10,7 @@ env_packages = "#{node[:fabric_deployment][:env_package_dir]}/#{environment_id}"
 # Which package should be deployed?
 #
 current_package_file = "#{env_packages}/current-version.txt"
-if FileTest.exists?(current_package_file)
+if FileTest.size?(current_package_file)
 	set[:fabric_deployment][:packages][:current] = File.open(current_package_file, "r").gets.first.chomp
 else
 	set[:fabric_deployment][:packages][:current] = ""
@@ -20,7 +20,7 @@ end
 # Which package actually is deployed?
 #
 deployed_here_file = "#{env_packages}/deployed-#{node[:hostname]}.txt"
-if FileTest.exists?(deployed_here_file)
+if FileTest.size?(deployed_here_file)
 	set[:fabric_deployment][:packages][:deployed] = File.open(deployed_here_file, "r").gets.first.chomp
 else
 	set[:fabric_deployment][:packages][:deployed] = ""
@@ -30,7 +30,7 @@ end
 # What is the database schema version?
 #
 database_schema_file = "#{env_packages}/db-schema-version.txt"
-if FileTest.exists?(database_schema_file)
+if FileTest.size?(database_schema_file)
 	set[:fabric_deployment][:packages][:dbschema] = File.open(database_schema_file, "r").gets.first.chomp
 else
 	set[:fabric_deployment][:packages][:dbschema] = ""
