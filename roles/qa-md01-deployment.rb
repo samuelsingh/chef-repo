36d13793d800f4215db3010cc1cd6c2fde87beeb
kief <kief@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-10-12 15:50:11 +0000

    PROJ-17 Swapping qa-md01 over to stage-db-01 since 02 has disk space issues
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2237 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/qa-md01-deployment.rb
+++ roles/qa-md01-deployment.rb
@@ -3,10 +3,11 @@ description "Configuration specific to a given QA deployment of Map Display"
 
 override_attributes(
   "map_display" => {
-    "version" => "2.7.0-2010-10-01_13-10-07-36704",
+    "version" => "0.0",
     "md_fqdn" =>  "qa-md01.map-cloud-01.eu",
-    "dbhost" => "stage-db-02.map-cloud-01.eu",
-    "dbname" => "qa_md01"
+    "dbhost" => "stage-db-01.map-cloud-01.eu",
+    "dbname" => "qa_md01",
+    "dbpass" => "MtMUs3r"	
   },
   "fabric_deployment" => {
     "environment" => "qa-md01"
