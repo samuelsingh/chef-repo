051d930359b17e90b95b6956df0709a3d78b4852
kief <kief@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-10-12 15:58:31 +0000

    PROJ-17 Twiddle to version number.
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2238 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/qa-md01-deployment.rb
+++ roles/qa-md01-deployment.rb
@@ -3,7 +3,7 @@ description "Configuration specific to a given QA deployment of Map Display"
 
 override_attributes(
   "map_display" => {
-    "version" => "0.0",
+    "version" => "0.0.0",
     "md_fqdn" =>  "qa-md01.map-cloud-01.eu",
     "dbhost" => "stage-db-01.map-cloud-01.eu",
     "dbname" => "qa_md01",
