26005c4b45b926a15306506d2345c5ef94a0e33d
kief <kief@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-10-12 15:12:35 +0000

    PROJ-17 Added vhosts for qa-md01 and qa-md02. Added them to the staging web server, arguably might be better to have a separate one.
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2236 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/stage-md-vhosts.rb
+++ roles/stage-md-vhosts.rb
@@ -21,6 +21,22 @@ override_attributes(
       "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause.",
       "appserver" => "stage-euromd-app-02.map-cloud-01.eu",
       "lb_alive_port" => 0
+    },
+    "qa-md01.map-cloud-01.eu" => {
+      "srv_aliases" => [],
+      "deploy_dir" => "/var/shared/fabric-pipeline/deploy/qa-md01/DEPLOYED",
+      "holding_page" => "false",
+      "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause.",
+      "appserver" => "qa-md01-app-01.map-cloud-01.eu",
+      "lb_alive_port" => 0
+    },
+    "qa-md02.map-cloud-01.eu" => {
+      "srv_aliases" => [],
+      "deploy_dir" => "/var/shared/fabric-pipeline/deploy/qa-md02/DEPLOYED",
+      "holding_page" => "false",
+      "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause.",
+      "appserver" => "qa-md02-app-01.map-cloud-01.eu",
+      "lb_alive_port" => 0
     }
   }
 )
