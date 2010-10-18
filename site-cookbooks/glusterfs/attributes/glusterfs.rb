c17b21d61d27edb7b8b298e9016833f886b28d5e
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-10-18 17:12:36 +0000

    Adding experimental glusterfs parameters
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2251 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/glusterfs/attributes/glusterfs.rb
+++ site-cookbooks/glusterfs/attributes/glusterfs.rb
@@ -2,4 +2,8 @@ if recipe?("glusterfs::server")
   set[:glusterfs][:server] = "true"
 end
 
+if recipe?("glusterfs::client")
+  set_unless[:glusterfs][:client][:experimental] = "false"
+end
+
   set_unless[:glusterfs][:mounts] = Array.new
