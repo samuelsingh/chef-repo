fdf518947442c877ba12ac1cd90b5baeb7f4e2ba
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-10-20 22:19:10 +0000

    Updating hgpe-prod-version role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2255 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/hgpe-prod-version.rb
+++ roles/hgpe-prod-version.rb
@@ -5,7 +5,7 @@ run_list "recipe[prod-server]"
 
 override_attributes(
   "hg_mde" => {
-    "hg_version" =>  "choices.R12.2-Issue_3_2010-100730.183004.769.d1v1r46-August_2010-100920.093349.423.d3v17r4247_3",
-    "mde_version" =>  "evidence.R4.1-Issue_3_2010-100730.183004.769.d1v1r46-August_2010-100920.093349.423.d3v17r4247_3"
+    "hg_version" =>  "choices.R12.2-Issue_3_2010-100730.183004.769.d1v1r46-September_2010_101018.130659.518.d3v17r4255-hudson-HG-build-appfuse-34-36875",
+    "mde_version" =>  "evidence.R4.1-Issue_3_2010-100730.183004.769.d1v1r46-September_2010_101018.130659.518.d3v17r4255-hudson-HG-build-appfuse-34-36875"
   }
 )
