624caf465b51de4c880ec591cab7a2905092bb84
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-10-24 15:52:26 +0000

    Adding gluster experimental parameters to stage-euromd-appserver
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2265 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/stage-euromd-appserver.rb
+++ roles/stage-euromd-appserver.rb
@@ -3,4 +3,16 @@ description "Configures Stage MD application server"
 
 run_list "recipe[java]", "recipe[tomcat]", "recipe[map-display]", "recipe[map-display::contentloader]"
 
-override_attributes "map_display" => {"deploy_dir" =>  "/var/shared/deployment/stage/euro-md","dbhost" => "stage-db-02.map-cloud-01.eu","dbuser" => "mtmuser","dbpass" => "medic1"}
+override_attributes(
+  "map_display" => {
+    "deploy_dir" =>  "/var/shared/deployment/stage/euro-md",
+    "dbhost" => "stage-db-02.map-cloud-01.eu",
+    "dbuser" => "mtmuser",
+    "dbpass" => "medic1"
+  },
+  "glusterfs" => {
+    "client" => {
+      "experimental" => "true"
+    }
+  }
+)
