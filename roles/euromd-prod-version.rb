abe30d5af90dce75279ff1c7de099b12cf12c8a4
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-10-28 13:36:34 +0000

    Update to roles
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2276 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/euromd-prod-version.rb
@@ -0,0 +1,12 @@
+name "euromd-live-version"
+description "MD staging version snippet"
+
+# This should change to recipe[prod-server] when Euro MD
+# goes live.
+run_list "recipe[stage-server]"
+
+override_attributes(
+  "map_display" => {
+    "version" => "2.6.5.ALL.12.36722"
+  }
+)
