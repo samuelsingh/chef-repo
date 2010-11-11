64ce07e7daa4aef799964a141509c2696014c14d
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-11-11 11:07:02 +0000

    Pushing changes to hg-mde
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2315 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/hg-mde/attributes/hg-mde.rb
+++ site-cookbooks/hg-mde/attributes/hg-mde.rb
@@ -1,5 +1,4 @@
 # Attributes for the hg-mde cookbook
 
 # Set here to fix odd behaviour with chef-solo 
-set[:tomcat][:ajp_ports] = [ 9001, 9002 ]
-
+#set[:tomcat][:ajp_ports] = [ 9001, 9002 ]
--- site-cookbooks/hg-mde/recipes/default.rb
+++ site-cookbooks/hg-mde/recipes/default.rb
@@ -8,6 +8,8 @@
 path9001 = "/var/tomcat/server9001"
 path9002 = "/var/tomcat/server9002"
 
+log node[:tomcat][:ajp_ports]
+
 template "#{path9001}/etc/java_opts.conf" do
   source "java_opts.conf.erb"
   mode 0755
@@ -34,4 +36,18 @@ template "#{path9002}/conf/Catalina/localhost/evidence.xml" do
   source "evidence.xml.erb"
   mode 0755
   only_if "test -d #{path9002}/conf"
+end
+
+directory "#{path9001}/webapps" do
+  mode 0755
+  owner "tomcat"
+  group "tomcat"
+  action :create
+end
+
+directory "#{path9002}/webapps" do
+  mode 0755
+  owner "tomcat"
+  group "tomcat"
+  action :create
 end
