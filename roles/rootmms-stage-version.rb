236b5a4d942addf926dc22017358db1789554b5d
jasong <jasong@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-11-12 15:04:18 +0000

    update versions for bugfixes
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2323 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/rootmms-stage-version.rb
+++ roles/rootmms-stage-version.rb
@@ -5,7 +5,7 @@ run_list "recipe[stage-server]"
 
 override_attributes(
   "mms" => {
-    "version" => "2.7.0.ALL.17.37067",
+    "version" => "2.7.0.ALL.18.37104",
     "me_version" => "7.7.0",
     "dict_version" => "0.2"
   }
