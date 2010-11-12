91dc4ec22c28e7920c7432ce85ac56bc6040ec94
gerardo <gerardo@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-11-12 09:20:47 +0000

    update hgpe-stage-version to new healthguides/mde wars
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2319 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/hgpe-stage-version.rb
+++ roles/hgpe-stage-version.rb
@@ -5,7 +5,7 @@ run_list "recipe[stage-server]"
 
 override_attributes(
  "hg_mde" => {
-   "hg_version" =>  "choices.R13.1-Issue_3a_2010-100730.183004.769.d1v1r46-September_2010_101018.130659.518.d3v17r4255-hudson-HG-build-appfuse-40-37037",
-    "mde_version" => "evidence.R5.1-Issue_3a_2010-100730.183004.769.d1v1r46-September_2010_101018.130659.518.d3v17r4255-hudson-HG-build-appfuse-40-37037"
+   "hg_version" =>  "choices.r13.1.hudson-47.war",
+    "mde_version" => "evidence.r5.1.hudson-47.war"
   }
 )
