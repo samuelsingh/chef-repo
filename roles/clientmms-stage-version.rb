67767b9ea2acac116f982e54f3305fc9184f8642
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-11-17 11:03:00 +0000

    Adding clientmms-stage-version role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2334 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/clientmms-stage-version.rb
@@ -0,0 +1,12 @@
+name "clientmms-stage-version"
+description "Root MMS staging version snippet"
+
+run_list "recipe[stage-server]"
+
+override_attributes(
+  "mms" => {
+    "version" => "2.7.0.ALL.18.37104",
+    "me_version" => "7.7.0",
+    "dict_version" => "0.2"
+  }
+)
