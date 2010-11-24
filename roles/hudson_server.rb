c008e46df90c27dfdaba4a7ca41722bdbaed6a3e
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-11-24 22:15:29 +0000

    Updating hudson server role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2350 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/hudson_server.rb
+++ roles/hudson_server.rb
@@ -3,7 +3,15 @@ description "Map of Medicine Hudson deployment server"
 
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 # run_list "recipe[zones]", "recipe[nfs_mounts]"
-run_list "recipe[java]", "recipe[tomcat]", "recipe[hudson_server]"
+run_list "recipe[java]", "recipe[tomcat]", "recipe[hudson_server]", "recipe[nexus]"
 
-#override_attributes "hudson_server" => {"version" =>  "hudson-1.377" }
-
+override_attributes(
+  "tomcat" => {
+    "unpackwars" => "true",
+    "ajp_ports" => [9001,9002]
+  },
+  "nexus" => {
+    "war" =>  "nexus-webapp-1.8.0.1.war",
+    "home" =>  "/var/shared/deployment/nexus/wars"
+  }
+)
