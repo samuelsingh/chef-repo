d90184a3f214fae30e85d3bad3cf3db276bb0a0c
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-11-26 15:55:05 +0000

    First pass of sonar
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2362 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/hudson_server.rb
+++ roles/hudson_server.rb
@@ -3,7 +3,7 @@ description "Map of Medicine Hudson deployment server"
 
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 # run_list "recipe[zones]", "recipe[nfs_mounts]"
-run_list "recipe[java]", "recipe[tomcat]", "recipe[hudson_server]", "recipe[nexus]"
+run_list "recipe[java]", "recipe[tomcat]", "recipe[hudson_server]", "recipe[nexus]", "recipe[sonar]", "recipe[mysql::server]"
 
 override_attributes(
   "tomcat" => {
@@ -13,5 +13,13 @@ override_attributes(
   "nexus" => {
     "war" =>  "nexus-webapp-1.8.0.1.war",
     "home" =>  "/var/shared/deployment/nexus"
+  },
+  "sonar" => {
+    "war" =>  "nexus-webapp-1.8.0.1.war",
+    "home" =>  "/var/hudson/sonar"
+  },
+  "mysql" => {
+    "server_root_password" =>  "nexus-webapp-1.8.0.1.war",
+    "datadir" =>  "/var/hudson/sonar/mysql"
   }
 )
