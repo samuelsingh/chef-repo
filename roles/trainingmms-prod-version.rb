46d8433f439c7ea981a3bcc817173464583b0042
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-12-05 19:36:10 +0000

    Updating trainingmms-prod-version.rb to 2.7
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2384 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/trainingmms-prod-version.rb
+++ roles/trainingmms-prod-version.rb
@@ -5,8 +5,8 @@ run_list "recipe[prod-server]"
 
 override_attributes(
   "mms" => {
-    "version" => "2.6.5.ALL.12.36722",
-    "me_version" => "7.6.9",
+    "version" => "2.7.0.ALL.25.37384",
+    "me_version" => "7.7.0",
     "dict_version" => "0.2"
   }
 )
