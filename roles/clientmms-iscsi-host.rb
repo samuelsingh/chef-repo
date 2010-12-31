8cd6513cda8446ac85e43f1352530918f318b48a
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2010-12-31 16:41:45 +0000

    Adding clientmms-iscsi-host role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2418 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/clientmms-iscsi-host.rb
@@ -0,0 +1,21 @@
+name "clientmms-iscsi-host"
+description "Configures iscsi host for Client MMS"
+
+run_list "recipe[prod-server]","recipe[aws]","recipe[tgt-iscsi]"
+
+override_attributes(
+  "aws" => {
+    "aws_access_key" => "AKIAIB5QZCFE3LUGZHVA",
+    "aws_secret_access_key" => "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"
+  },
+  "tgt_iscsi" => {
+    "mounts" => [
+      "vol-e6f02b8f,/dev/sdd",
+      "vol-f8f02b91,/dev/sde",
+      "vol-faf02b93,/dev/sdf",
+      "vol-f4f02b9d,/dev/sdg",
+      "vol-c8f02ba1,/dev/sdh",
+      "vol-caf02ba3,/dev/sdi"
+    ]
+  }
+)
