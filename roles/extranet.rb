f0b1a75541de3343652df9d299771b754ef4e0c1
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-01-19 21:40:25 +0000

    Adding extranet recipe
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2497 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/extranet.rb
@@ -0,0 +1,27 @@
+name "extranet"
+description "Adds http/ftp access for extranet.mapofmedicine.com"
+run_list "recipe[vsftpd]"
+
+override_attributes(
+  "vsftpd" => {
+    "ftp_base" => "/var/shared/ftp",
+    "users" => {
+      "csc" => {
+        "uid" => "10500",
+        "pwd_hash" => "$6$sq0MlFJZ$Y1UijZDhQwn0pQmx.n8yV6cmR/OyzOCjqpvr9xU/X9X65ENAjaakj2kEO.lY6t3kLZzXOnjZUhb7invcpi8Lo0"
+      },
+      "dev" => {
+        "uid" => "10501",
+        "pwd_hash" => "$6$ST/GLqZF$Me9Syc4feLlW87j57ROi.KwIR8qv2vvlumVYJuVCqK1GrM70T4Zp0IA30Am/4CcsIkeunBfNaHHdz.LeyOmTm1"
+      },
+      "nhschoices" => {
+        "uid" => "10502",
+        "pwd_hash" => "$6$VDEksNPO$j0cBsmcJvAar3/nfRx16sGmOcUwRjTTIiJkFT92.GS6TqC6pDHWbFBSc7dSDe741ve0gNRzQpQvind9PmPi7C."
+      },
+      "vitalpac" => {
+        "uid" => "10503",
+        "pwd_hash" => "$6$YY6DLSp0$yaUBh6lLEPtNCNKCyaE0oQ7hhAE48O9aj4XhaH.5qvVFwF88q.nPsT3.eekIkmIZ18EiCxQLWaXFBknu.ZIAP."
+      }
+    }
+  }
+)
