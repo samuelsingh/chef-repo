fb6ff6c14e88f1da52b0a34624c60aca44d1bb03
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-01-20 16:39:18 +0000

    Adding vsftpd::http
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2509 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ site-cookbooks/vsftpd/recipes/http.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: vsftpd
+# Recipe:: http
+#
+# Copyright 2011, Map of Medicine
+#
+
+include_recipe "apache2"
+
+ftp_users = node[:vsftpd][:users]
+ftp_base = node[:vsftpd][:ftp_base]
+ftp_host = node[:vsftpd][:ftp_host]
+
+["libapache2-mod-auth-pam"].each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+template "#{node[:apache][:dir]}/sites-available/#{hostname}.conf" do
+  source "vsftpd-vhost.conf.erb"
+  mode 0644
+  variables(
+    :users => ftp_users.map{|k,v| k},
+    :hostname => ftp_host,
+    :ftp_base => ftp_base
+  )
+  notifies :reload, resources(:service => "apache2")
+end
