8797f9286da6454d068d5514abf19d46cde75fe5
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-01-25 20:12:49 +0000

    Updating nexus role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2527 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/hudson_server.rb
+++ roles/hudson_server.rb
@@ -4,16 +4,25 @@ description "Map of Medicine Hudson deployment server"
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 # run_list "recipe[zones]", "recipe[nfs_mounts]"
 # run_list "recipe[java]", "recipe[tomcat]", "recipe[hudson_server]", "recipe[nexus-sonar]", "recipe[mysql::server]"
-run_list "recipe[java]", "recipe[tomcat]", "recipe[hudson_server]", "recipe[nexus]"
+run_list "recipe[java]", "recipe[tomcat]", "recipe[hudson_server]", "recipe[aws]", "recipe[nexus::ebs]", "recipe[nexus]"
 
 override_attributes(
   "tomcat" => {
     "unpackwars" => "true",
     "ajp_ports" => [9001,9002]
   },
+  "aws" => {
+    "aws_access_key" => "AKIAIB5QZCFE3LUGZHVA",
+    "aws_secret_access_key" => "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"
+  },
   "nexus" => {
     "war" =>  "nexus-webapp-1.8.0.1.war",
-    "home" =>  "/var/shared/deployment/nexus"
+    "home" =>  "/var/nexus",
+    "ebs" => {
+      "name" => "set-me",
+      "device" => "/dev/sdi",
+      "fstype" => "ext3"
+    }
   },
   "sonar" => {
     "war" =>  "sonar-2.4.1.war",
