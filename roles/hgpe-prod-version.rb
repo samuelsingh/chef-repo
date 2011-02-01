db7db1188fed9a44c9b7b1699e0ce91cd0fa1dcc
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-02-01 12:46:56 +0000

    Updating HG/MDE version
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2562 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/hgpe-prod-version.rb
+++ roles/hgpe-prod-version.rb
@@ -4,8 +4,8 @@ description "Specifies the HG and MDE versions currently used in production"
 run_list "recipe[prod-server]"
 
 override_attributes(
-  "hg_mde" => {
-    "hg_version" =>  "choices.R12.2-Issue_3_2010-100730.183004.769.d1v1r46-September_2010_101018.130659.518.d3v17r4255-hudson-HG-build-appfuse-34-36875",
-    "mde_version" =>  "evidence.R4.1-Issue_3_2010-100730.183004.769.d1v1r46-September_2010_101018.130659.518.d3v17r4255-hudson-HG-build-appfuse-34-36875"
+ "hg_mde" => {
+   "hg_version" =>  "choices.r13.1.hudson-55",
+    "mde_version" => "evidence.r5.1.hudson-55"
   }
 )
