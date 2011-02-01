40471cb8d470442ee59dd822fafff05b8d9942f4
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-02-01 22:42:17 +0000

    Adding md-server.json
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2570 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- solo/solo.rb
+++ solo/solo.rb
@@ -1,3 +1,3 @@
-cookbook_path	[ "/var/chef/cookbooks", "/var/chef/site-cookbooks" ]
+cookbook_path	[ "/home/andrew/svn/chef-experimental/chef-repo/site-cookbooks" ]
 file_cache_path	"/tmp/chef-solo"
-role_path "/var/chef/roles"
+role_path "/home/andrew/svn/chef-experimental/chef-repo/roles"
