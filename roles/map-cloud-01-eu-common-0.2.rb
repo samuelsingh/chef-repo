125a91861459f1145e7e70cf2172fd76745f5adc
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-02-06 18:23:25 +0000

    Adding map-cloud-01-eu-common-0.2 role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2575 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/map-cloud-01-eu-common-0.2.rb
@@ -0,0 +1,24 @@
+name "map-cloud-01-eu-common-0.2"
+description "Configuration common to all machines in map-cloud-01.eu. V0.2"
+run_list "recipe[zones]", "recipe[glusterfs::client]", "recipe[standard_users]", "recipe[sudo]", "recipe[openssh-server-ec2]", "recipe[exim::satellite]", "recipe[extra-repos]", "recipe[mom-scripts]", "recipe[ntp]", "recipe[snmpd]", "recipe[zenoss::client]", "role[rsyslog-mom-client]"
+
+override_attributes(
+  "zones" => {
+    "name" =>  "euaws"
+  },
+  "snmpd" => {
+    "community" => "ufahji3K",
+    "syslocation" => "map-cloud-01.eu, amazon eu-west-1",
+    "syscontact" => "systems@mapofmedicine.com"
+  },
+  "ec2" => {
+    "ec2_access_key" => "",
+    "ec2_secret_key" => ""
+  },
+  "glusterfs" => {
+    "client" => {
+      "stable" => "true",
+      "mounts" => ["shared,/var/shared","eph_share,/var/eph_share"]
+    }
+  }
+)
