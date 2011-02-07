45b0f91d451be1d4d07606aa4d16a641442dde0c
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-02-07 18:41:12 +0000

    Adding basic recipe to vagrant-helpers for the devops scripts
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2593 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/vagrant-helpers/recipes/default.rb
+++ site-cookbooks/vagrant-helpers/recipes/default.rb
@@ -4,15 +4,9 @@
 #
 # Copyright 2011, Map of Medicine
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# 
-#     http://www.apache.org/licenses/LICENSE-2.0
-# 
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
+remote_directory "/usr/local/devops" do
+  source "devops"
+  files_mode "0755"
+  mode "0755"
+end
