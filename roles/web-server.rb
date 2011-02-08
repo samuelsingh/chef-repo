7a75751ea709911544d63432ba8f44dd6a7d3b95
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-02-08 20:20:48 +0000

    Adding apache::rotate_logs
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2596 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/web-server.rb
+++ roles/web-server.rb
@@ -1,8 +1,6 @@
 name "web-server"
 description "Configuration common to all hosts"
-# List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
-# run_list "recipe[zones]", "recipe[nfs_mounts]"
-run_list "recipe[apache2]", "recipe[apache2::mod_rewrite]", "recipe[apache2::mod_proxy_ajp]", "recipe[apache2::mod_headers]", "recipe[apache2::mod_dav]", "recipe[apache2::mod_ssl]", "recipe[apache2::mod_authnz_ldap]", "recipe[apache2::mod_proxy_connect]", "recipe[apache2::mod_ldap]", "recipe[apache2::mod_php5]", "recipe[apache2::mod_proxy_http]", "recipe[apache2::mod_status]", "recipe[apache2::mod_cgi]", "recipe[apache2::mod_log_config]", "recipe[apache2::mod_expires]", "recipe[apache2::mod_include]", "recipe[apache2::mod_log_forensic]", "recipe[apache2::mod_info]"
+run_list "recipe[apache2]", "recipe[apache2::mod_rewrite]", "recipe[apache2::mod_proxy_ajp]", "recipe[apache2::mod_headers]", "recipe[apache2::mod_dav]", "recipe[apache2::mod_ssl]", "recipe[apache2::mod_authnz_ldap]", "recipe[apache2::mod_proxy_connect]", "recipe[apache2::mod_ldap]", "recipe[apache2::mod_php5]", "recipe[apache2::mod_proxy_http]", "recipe[apache2::mod_status]", "recipe[apache2::mod_cgi]", "recipe[apache2::mod_log_config]", "recipe[apache2::mod_expires]", "recipe[apache2::mod_include]", "recipe[apache2::mod_log_forensic]", "recipe[apache2::mod_info]", "recipe[apache2::rotate_logs]"
 
 override_attributes(
   "apache" => {
@@ -15,6 +13,8 @@ override_attributes(
       "86.166.206.203",   # Gerard's home
       "82.34.165.170",    # Andrew's home
       "78.105.68.171"     # Patrick's home
-    ]
+    ],
+    "log_bak_dir" => "/var/shared/backups",
+    "log_rotate_dir" => "/var/shared/rotated-logs"
   }
 )
