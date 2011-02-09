25f28b0b28c00f6582002246f52f974ad2906bd9
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-02-09 13:18:56 +0000

    Adding tomcat::rotate_logs
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2599 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ site-cookbooks/tomcat/recipes/rotate_logs.rb
@@ -0,0 +1,48 @@
+#
+# Cookbook Name:: tomcat
+# Recipe:: helper-scripts
+#
+# Copyright 2011, Map of Medicine
+#
+
+bin = '/usr/local/bin'
+sbin = '/usr/local/sbin'
+
+tomcat_srv = node[:tomcat][:tomcat_srv_dir]
+rotate_base = node[:tomcat][:log_rotate_dir]
+tomcat_ports = node[:tomcat][:ajp_ports]
+
+
+# Installs dependent packages
+["lsof"].each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+lsof_bin = value_for_platform(
+  [ "ubuntu", "debian" ] => {
+    "default" => "/usr/bin/lsof"
+  },
+  [ "redhat", "centos", "fedora" ] => {
+    "default" => "/usr/sbin/lsof"
+  },
+  "default" => "/usr/bin/lsof"
+)
+
+template "#{sbin}/gather-tomcat-logs.rb" do
+  source "gather-tomcat-logs.rb.erb"
+  mode 0755
+  variables(
+    :bin => bin,
+    :tomcat_srv => tomcat_srv,
+    :rotate_base => rotate_base,
+    :lsof_bin => lsof_bin
+  )
+end
+
+#cron "rotate-httpd-logs" do
+#  hour node[:tomcat][:rotate_hour]
+#  minute node[:tomcat][:rotate_min]
+#  command "#{sbin}/gather-tomcat-logs.rb"
+#end
