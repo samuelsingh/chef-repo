7b0b7e93083dac91bc6a511119aed61428ca6bd2
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-02-11 16:50:27 +0000

    Adding variable interpolation
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2619 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- solo/solo.rb
+++ solo/solo.rb
@@ -1,3 +1,3 @@
-cookbook_path	[ "/home/andrew/svn/chef-experimental/chef-repo/site-cookbooks" ]
+cookbook_path	[ "@WORKSPACE@/chef-repo/site-cookbooks" ]
 file_cache_path	"/tmp/chef-solo"
-role_path "/home/andrew/svn/chef-experimental/chef-repo/roles"
+role_path "@WORKSPACE@/chef-repo/roles"
