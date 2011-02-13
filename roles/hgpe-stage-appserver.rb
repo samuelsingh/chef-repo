bc63cc5800a3311d30cae42b3b4f662966b44b54
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-02-13 19:01:41 +0000

    Adding hudson agent to the hg staging appserver role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2629 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/hgpe-stage-appserver.rb
+++ roles/hgpe-stage-appserver.rb
@@ -1,7 +1,7 @@
 name "hgpe-stage-appserver"
 description "Configures HG / MDE application server"
 
-run_list "recipe[java]", "recipe[tomcat]", "recipe[hg-mde]"
+run_list "recipe[java]", "recipe[tomcat]", "recipe[hg-mde]", "recipe[hudson_agent]"
 
 override_attributes(
   "tomcat" => {
@@ -11,5 +11,9 @@ override_attributes(
     "hg_war_path" =>  "/var/shared/deployment/stage/healthguides/wars",
     "mde_war_path" =>  "/var/shared/deployment/stage/mde/wars",
     "specify_docbase" => "false"
+  },
+  "hudson_agent" => {
+    "user" => "tomcat",
+    "group" => "tomcat"
   }
 )
