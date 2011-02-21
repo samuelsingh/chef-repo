ec311ee58d494bd8b6021e5dc54807bb2cc06f6a
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-02-21 23:29:19 +0000

    bugfix
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2650 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/tomcat/recipes/rotate_logs.rb
+++ site-cookbooks/tomcat/recipes/rotate_logs.rb
@@ -7,11 +7,11 @@
 
 bin = '/usr/local/bin'
 sbin = '/usr/local/sbin'
+lib = '/usr/local/lib'
 
 tomcat_srv = node[:tomcat][:srv_dir]
 rotate_base = node[:tomcat][:log_rotate_dir]
 tomcat_ports = node[:tomcat][:ajp_ports].join(',')
-
 
 # Installs dependent packages
 ["lsof"].each do |pkg|
@@ -30,11 +30,15 @@ lsof_bin = value_for_platform(
   "default" => "/usr/bin/lsof"
 )
 
+remote_file "#{lib}/gather-logs.rb" do
+  source "gather-logs.rb"
+end
+
 template "#{sbin}/gather-tomcat-logs.rb" do
   source "gather-tomcat-logs.rb.erb"
   mode 0755
   variables(
-    :bin => bin,
+    :lib => lib,
     :tomcat_srv => tomcat_srv,
     :rotate_base => rotate_base,
     :tomcat_ports => tomcat_ports,
