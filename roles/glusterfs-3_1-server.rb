f38a9ea8d139bc2ee80ab4355f9bb1d49b6bad12
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-02-23 14:51:45 +0000

    Adding glusterfs-3_1-server.rb
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2670 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/glusterfs-3_1-server.rb
@@ -0,0 +1,20 @@
+name "glusterfs-3_1-server"
+description "Configures glusterfs storage node"
+
+# run_list "recipe[prod-server]","recipe[aws]","recipe[glusterfs::ebs]","recipe[glusterfs::ephemeral]","recipe[glusterfs::server]","recipe[glusterfs::cifs]"
+run_list "recipe[prod-server]","recipe[aws]","recipe[glusterfs_3_1::ephemeral]","recipe[glusterfs_3_1::server]"
+
+override_attributes(
+  "aws" => {
+    "aws_access_key" => "AKIAIB5QZCFE3LUGZHVA",
+    "aws_secret_access_key" => "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"
+  }
+  "glusterfs" => {
+    "server_eph_volumes" => {
+      "eph_share" => {
+        "device" => "/dev/sdc",
+        "fstype" => "ext3"
+      }
+    }
+  }
+)
