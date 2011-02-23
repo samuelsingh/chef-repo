aa6b4c05bbd43774111f675dcd492a377a6e694e
gerardo <gerardo@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-02-23 19:03:54 +0000

    updating hgpe-prod-version.rb fot latest hg/mde content xxx.hudson-75.war
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2684 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/hgpe-prod-version.rb
+++ roles/hgpe-prod-version.rb
@@ -5,7 +5,7 @@ run_list "recipe[prod-server]"
 
 override_attributes(
  "hg_mde" => {
-   "hg_version" =>  "choices.r13.3.hudson-72.war",
-    "mde_version" => "evidence.r5.3.hudson-72.war"
+   "hg_version" =>  "choices.r13.3.hudson-75.war",
+    "mde_version" => "evidence.r5.3.hudson-75.war"
   }
 )
