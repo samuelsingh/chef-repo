a5bed06e236e06eef341852379b014322a6bd0a3
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-03-09 14:59:43 +0000

    Adding legacy-stage-sites
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2721 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/legacy-stage-sites.rb
@@ -0,0 +1,16 @@
+name "legacy-stage-sites"
+description "Configures legacy dev/stage sites"
+# List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
+# run_list "recipe[zones]", "recipe[nfs_mounts]"
+run_list "recipe[generic_vhost]"
+
+override_attributes(
+  "generic_vhost" => {
+    "stage-web.mapofmedicine.com" => {
+      "srv_aliases" => ["stagewebsite.mapofmedicine.com"],
+      "docroot" => "/var/shared/deployment/dev/stage-web",
+      "holding_page" => "false",
+      "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause.",
+    }
+  }
+)
