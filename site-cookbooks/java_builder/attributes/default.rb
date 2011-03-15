ca97e9b91ca55b2e733e5826e6ff8d5d42265b53
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-03-15 12:22:42 +0000

    Making default RAM more realistic
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2734 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/java_builder/attributes/default.rb
+++ site-cookbooks/java_builder/attributes/default.rb
@@ -2,6 +2,6 @@
 set[:java_builder][:install_files] = "/var/shared/global/infrastructure/standard/build-server"
 set[:java_builder][:legacy_ant_version] = "apache-ant-1.6.5"
 
-set[:hudson_agent][:jvm][:max_heap] = "1250m"
-set[:hudson_agent][:jvm][:start_heap] = "1250m"
+set[:hudson_agent][:jvm][:max_heap] = "256m"
+set[:hudson_agent][:jvm][:start_heap] = "256m"
 
