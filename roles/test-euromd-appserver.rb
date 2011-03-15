f7a0adc727211b4da23b5ea6ff5412419843439a
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-03-15 14:15:04 +0000

    Adding test-euromd-appserver role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2735 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/test-euromd-appserver.rb
@@ -0,0 +1,24 @@
+name "test-euromd-appserver"
+description "Configures test MD application server"
+
+run_list "role[tomcat]", "recipe[map-display-2]", "recipe[smoke_test]"
+
+override_attributes(
+  "map_display" => {
+    "md_fqdn" => "euromd.test.mapofmedicine.com",
+    "dbhost" => "stage-db-01.map-cloud-01.eu",
+    "dbname" => "md-test",
+    "dbuser" => "mtmuser",
+    "dbpass" => "MtMUs3r",
+    "save_ram" => "true"
+  },
+  "glusterfs" => {
+    "client" => {
+      "stable" => "true"
+    }
+  },
+  "tomcat" => {
+    "srv_dir" => "/var/tomcat",
+    "log_rotate_dir" => "/var/shared/rotated-logs"
+  }
+)
