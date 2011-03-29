3e58aca6c8d20a4af1599278068b6e4db6571879
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-03-29 11:24:41 +0000

    Updating ntt role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2764 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/ntt-london-common.rb
+++ roles/ntt-london-common.rb
@@ -1,3 +1,3 @@
 name "ntt-london-common"
 description "Configuration common to all machines in map-cloud-01.eu. V0.2"
-run_list "recipe[standard_users]", "recipe[sudo]", "recipe[extra-repos]", "recipe[mom-scripts]"
+run_list "recipe[standard_users]", "recipe[extra-repos]"
