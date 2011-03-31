5c8a53c5cead365dceefbb9ca1c97c8e1baee418
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-03-31 14:17:54 +0000

    Updating DA
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2768 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/hgpe-prod-version.rb
+++ roles/hgpe-prod-version.rb
@@ -6,6 +6,6 @@ run_list "recipe[prod-server]"
 override_attributes(
  "hg_mde" => {
    "hg_version" =>  "choices.r13.4.hudson-81",
-    "mde_version" => "evidence.r5.4.hudson-81"
+    "mde_version" => "evidence.r5.5.hudson-87"
   }
 )
