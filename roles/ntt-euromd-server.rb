47ad6f11936ef5a4c87c9febd18f84f9776cb870
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-04-04 15:05:41 +0000

    Adding ntt-euromd-server role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2774 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/ntt-euromd-server.rb
@@ -0,0 +1,33 @@
+name "ntt-euromd-server"
+description "Configures Live MD application server"
+
+run_list "recipe[java]", "recipe[tomcat]", "recipe[map-display::application]", "recipe[map-display::vhost]", "recipe[map-display::lpa_vhost]"
+
+override_attributes(
+  "map_display" => {
+    "application" => {
+      "md_fqdn" =>  "app.mapofmedicine.com",
+      "dbhost" => "raspberry.mapofmedicine.com",
+      "dbname" => "live_mtmdb",
+      "dbuser" => "mtmuser",
+      "dbpass" => "medic1"
+    },
+    "vhost" => {
+      "app.mapofmedicine.com" => {
+        "srv_aliases" => ["app.mapofmedicine.info"],
+        "holding_page" => "false",
+        "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause.",
+        "appserver" => "127.0.0.1",
+        "lb_alive_port" => 0
+      }
+    },
+    "lpa_vhost" => {
+      "localcare.test.mapofmedicine.com" => {
+        "srv_aliases" => [],
+        "holding_page" => "false",
+        "appserver" => "127.0.0.1",
+        "lb_alive_port" => 0
+      }
+    }
+  }
+)
