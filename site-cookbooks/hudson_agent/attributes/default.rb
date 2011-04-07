956de34ebebab2cbc0363c3d0fb1e9ac2b05e208
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-04-07 14:24:43 +0000

    Trying to fix the hudson user weirdness
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2793 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/hudson_agent/attributes/default.rb
+++ site-cookbooks/hudson_agent/attributes/default.rb
@@ -2,8 +2,8 @@
 set[:hudson_agent][:server_url] = "http://hudson-01.map-cloud-01.eu:8080/hudson"
 set[:hudson_agent][:home_dir] = "/var/hudson-agent"
 
-set[:hudson_agent][:user] = "hudson"
-set[:hudson_agent][:group] = "hudson"
+set_unless[:hudson_agent][:user] = "hudson"
+set_unless[:hudson_agent][:group] = "hudson"
 
 set[:hudson_agent][:jvm][:max_heap] = "256m"
 set[:hudson_agent][:jvm][:start_heap] = "256m"
