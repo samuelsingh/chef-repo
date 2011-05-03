dc107ba181f31b365bf3885eca2936dcc7605785
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-05-03 10:10:03 +0000

    Added exim::satellite to ntt-london-common role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2825 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/ntt-london-common.rb
+++ roles/ntt-london-common.rb
@@ -1,3 +1,3 @@
 name "ntt-london-common"
 description "Configuration common to all machines in map-cloud-01.eu. V0.2"
-run_list "recipe[standard_users]", "recipe[extra-repos]"
+run_list "recipe[standard_users]", "recipe[exim::satellite]", "recipe[extra-repos]"
