a9946f2002c7538b138ad7b250225ba18eef3423
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-05-03 10:35:55 +0000

    Set smarthost in ntt-london-common role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2826 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/ntt-london-common.rb
+++ roles/ntt-london-common.rb
@@ -1,3 +1,10 @@
 name "ntt-london-common"
 description "Configuration common to all machines in map-cloud-01.eu. V0.2"
 run_list "recipe[standard_users]", "recipe[exim::satellite]", "recipe[extra-repos]"
+
+override_attributes(
+  "exim" => {
+    "smarthost" =>  "mail.map-cloud-01.eu"
+  }
+)
+
