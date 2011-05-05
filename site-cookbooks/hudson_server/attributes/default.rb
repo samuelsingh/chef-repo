094564192985adac386be358312df2e96e101027
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-05-05 11:09:46 +0000

    Updating hudson to jenkins
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2827 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/hudson_server/attributes/default.rb
+++ site-cookbooks/hudson_server/attributes/default.rb
@@ -1,5 +1,5 @@
 
-set[:hudson_server][:version]           = "hudson-current"
+set[:hudson_server][:version]           = "jenkins"
 set[:hudson_server][:install_path]      = "/var/tomcat/server9001"
 set[:hudson_server][:war_path]          = "/var/hudson/deploy-wars"
 set[:hudson_server][:home]              = "/var/hudson"
