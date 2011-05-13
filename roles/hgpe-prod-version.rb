21a4ff5d28449496eba17f6bb1d6f4bc66de0341
gerardo <gerardo@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-05-13 13:04:54 +0000

    updating choices and evidence
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2834 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/hgpe-prod-version.rb
+++ roles/hgpe-prod-version.rb
@@ -5,7 +5,7 @@ run_list "recipe[prod-server]"
 
 override_attributes(
  "hg_mde" => {
-   "hg_version" =>  "choices.r13.4.hudson-81",
-    "mde_version" => "evidence.r5.5.hudson-87"
+   "hg_version" =>  "choices.r13.5.hudson-94",
+    "mde_version" => "evidence.r5.5.hudson-94"
   }
 )
