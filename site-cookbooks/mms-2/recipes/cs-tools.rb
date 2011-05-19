1c3fa9163129a964f64df755c73e38d9c5726d8b
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-05-19 12:01:07 +0000

    Updating cs-tools recipe
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2851 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/mms-2/recipes/cs-tools.rb
+++ site-cookbooks/mms-2/recipes/cs-tools.rb
@@ -17,39 +17,44 @@
 # limitations under the License.
 #
 
-cstools    = "#{node[:mms][:common][:base]}/cs-tools"
-#version    = node[:mms][:version]
-#deploy_dir = node[:mms][:deploy_dir]
-mmpath     = "#{node[:mms][:common][:base]}/mapmanager"
-logpath    = "#{node[:mms][:common][:base]}/logs"
+mms_base            = node[:mms][:common][:base]
+content_in          = "#{mms_base}/content-in"
+content_out         = "#{mms_base}/content-out"
+cstools_base        = "#{mms_base}/cs-tools"
+log_base            = "#{mms_base}/logs"
+mapmanager_base     = "#{mms_base}/mapmanager"
+mom_base            = "#{mms_base}/mom"
+previewloader_base  = "#{mms_base}/previewloader"
 
 t_user = node[:tomcat][:user]
 t_group = node[:tomcat][:group]
 
-directory "#{cstools}/config"  do
+repo_home = "#{mapmanager_base}/repo"
+
+directory "#{cstools_base}/config"  do
   mode "0755"
   recursive true
   action :create
-  not_if "test -d #{cstools}/config"
+  not_if "test -d #{cstools_base}/config"
 end
 
-remote_file "#{cstools}/cs-tools" do
+remote_file "#{cstools_base}/cs-tools" do
   source "cs-tools/cs-tools.sh"
   mode "0755"
 end
 
-directory "#{logpath}/cs-tools"  do
+directory "#{log_base}/cs-tools"  do
   owner t_user
   group t_group
   mode "0755"
   recursive true
   action :create
-  not_if "test -d #{logpath}/cs-tools"
+  not_if "test -d #{log_base}/cs-tools"
 end
 
 # cs-tools doesn't actually use values from this
 #
-template "#{cstools}/config/mom.properties" do
+template "#{cstools_base}/config/mom.properties" do
   source "mom/mom.properties.erb"
   mode 0644
   variables(
@@ -58,34 +63,43 @@ template "#{cstools}/config/mom.properties" do
   )
 end
 
-template "#{cstools}/config/m2mr2-cs-deployment.properties" do
+template "#{cstools_base}/config/m2mr2-cs-deployment.properties" do
   source "cs-tools/m2mr2-cs-deployment.properties.erb"
   mode 0644
 end
 
-template "#{cstools}/config/repository.properties" do
+template "#{cstools_base}/config/repository.properties" do
   source "cs-tools/repository.properties.erb"
   mode 0644
   variables(
-    :mmpath => mmpath
+    :mapmanager_base => mapmanager_base
   )
 end
 
-link "#{cstools}/config/m2mr2-cs-base.properties"  do
-  to "#{node[:mms][:mapmanager][:path]}/config/m2mr2-cs-base.properties"
-  only_if "test -f #{node[:mms][:mapmanager][:path]}/config/m2mr2-cs-base.properties"
+template "#{cstools_base}/config/m2mr2-cs-base.properties" do
+  source "mapmanager/m2mr2-cs-base.properties.erb"
+  mode 0644
+  variables(
+    :mapmanager_base => mapmanager_base,
+    :repo_home => repo_home,
+    :content_out => content_out,
+    :previewloader_base => previewloader_base,
+    :deployment_name => 'cs-tools',
+    :previewhour => '1',
+    :previewmin => '1'
+  )
 end
 
-remote_file "#{cstools}/config/auto-approve.properties" do
+remote_file "#{cstools_base}/config/auto-approve.properties" do
   source "cs-tools/config/auto-approve.properties"
   mode 0644
 end
 
-template "#{cstools}/config/log4j.xml" do
+template "#{cstools_base}/config/log4j.xml" do
   source "cs-tools/log4j.xml.erb"
   mode 0644
   variables(
-    :logpath => logpath
+    :log_base => log_base
   )
 end
 
@@ -93,11 +107,11 @@ template "/etc/profile.d/mms-cstools.sh" do
   source "etc/profile.d/mms-cstools.sh.erb"
   mode 0755
   variables(
-    :cstools_path => cstools
+    :cstools_base => cstools_base
   )
 end
 
-remote_file "#{cstools}/config/m2mr2-cs-spring.properties" do
+remote_file "#{cstools_base}/config/m2mr2-cs-spring.properties" do
   source "cs-tools/config/m2mr2-cs-spring.properties"
   mode 0644
 end
