303209e8f0b25d17ef21b56a65adb8a76b4fd0af
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-05-23 19:16:57 +0000

    Adding the actual recipes :-s
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2867 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/ntt-euromd-server.rb
+++ roles/ntt-euromd-server.rb
@@ -1,7 +1,7 @@
 name "ntt-euromd-server"
 description "Configures Live MD application server"
 
-run_list "recipe[java]", "recipe[tomcat]", "role[web-server]", "recipe[map-display-3::application]", "recipe[map-display-3::contentloader]", "recipe[map-display-3::vhost]", "recipe[map-display-3::lpa_vhost]"
+run_list "recipe[java]", "recipe[tomcat]", "role[web-server]", "recipe[map-display-3::application]", "recipe[map-display-3::contentloader]", "recipe[map-display-3::vhost]", "recipe[map-display-3::lpa_vhost]", "recipe[map-display-3::rotate_logs]", "recipe[map-display-3::time_transactions]"
 
 override_attributes(
   "map_display" => {
