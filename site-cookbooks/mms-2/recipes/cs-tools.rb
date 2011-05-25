49cfe23bc8ecbee680081a768e3e1a750026958c
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-05-25 12:23:36 +0000

    Adding zipfile deployment to cs-tools
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2893 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/mms-2/recipes/cs-tools.rb
+++ site-cookbooks/mms-2/recipes/cs-tools.rb
@@ -25,21 +25,44 @@ mapmanager_base     = "#{mms_base}/mapmanager"
 mom_base            = "#{mms_base}/mom"
 previewloader_base  = "#{mms_base}/previewloader"
 
+dist_dir    = "#{mms_base}/dist"
+dist_zip    = "cs-tools.zip"
+
 t_user = node[:tomcat][:user]
 t_group = node[:tomcat][:group]
 
 repo_home = "#{mapmanager_base}/repo"
 
-directory "#{cstools_base}/config"  do
-  mode "0755"
-  recursive true
+## The below is a generic routine to deploy a zipfile. It does this by comparing an md5sum of the archive,
+## with the one stored, and re-deploys if the two don't match
+
+# Zipfiles placed here will be deployed by the next chef run
+directory "#{dist_dir}" do
   action :create
-  not_if "test -d #{cstools_base}/config"
+  recursive true
+  not_if "test -d #{dist_dir}"
+end
+
+if File.exists?("#{dist_dir}/#{dist_zip}")
+  
+  execute "deploy_dist" do
+    command "rm -rf #{cstools_base} && unzip #{dist_dir}/#{dist_zip} -d #{cstools_base} && chmod 755 #{cstools_base}/*.sh"
+    action :nothing
+  end
+  
+  # Checksum the contentloader
+  execute "checksum_contentloader" do
+    command "md5sum #{dist_dir}/#{dist_zip} > #{dist_dir}/#{dist_zip}.md5"
+    notifies :run, resources(:execute => "deploy_dist"), :immediately
+    not_if "md5sum -c --status #{dist_dir}/#{dist_zip}.md5 > /dev/null"
+  end
+  
 end
 
 remote_file "#{cstools_base}/cs-tools" do
   source "cs-tools/cs-tools.sh"
   mode "0755"
+  only_if "test -d #{cstools_base}"
 end
 
 directory "#{log_base}/cs-tools"  do
@@ -48,11 +71,9 @@ directory "#{log_base}/cs-tools"  do
   mode "0755"
   recursive true
   action :create
-  not_if "test -d #{log_base}/cs-tools"
 end
 
-# cs-tools doesn't actually use values from this
-#
+# cs-tools doesn't actually use values from this, but just requires these values as placeholders
 template "#{cstools_base}/config/mom.properties" do
   source "mom/mom.properties.erb"
   mode 0644
@@ -60,11 +81,13 @@ template "#{cstools_base}/config/mom.properties" do
     :mompath => "/tmp",
     :fqdn => "unset"
   )
+  only_if "test -d #{cstools_base}/config"
 end
 
 template "#{cstools_base}/config/m2mr2-cs-deployment.properties" do
   source "cs-tools/m2mr2-cs-deployment.properties.erb"
   mode 0644
+  only_if "test -d #{cstools_base}/config"
 end
 
 template "#{cstools_base}/config/repository.properties" do
@@ -73,6 +96,7 @@ template "#{cstools_base}/config/repository.properties" do
   variables(
     :mapmanager_base => mapmanager_base
   )
+  only_if "test -d #{cstools_base}/config"
 end
 
 template "#{cstools_base}/config/m2mr2-cs-base.properties" do
@@ -87,11 +111,13 @@ template "#{cstools_base}/config/m2mr2-cs-base.properties" do
     :previewhour => '1',
     :previewmin => '1'
   )
+  only_if "test -d #{cstools_base}/config"
 end
 
 remote_file "#{cstools_base}/config/auto-approve.properties" do
   source "cs-tools/config/auto-approve.properties"
   mode 0644
+  only_if "test -d #{cstools_base}/config"
 end
 
 template "#{cstools_base}/config/log4j.xml" do
@@ -100,6 +126,7 @@ template "#{cstools_base}/config/log4j.xml" do
   variables(
     :log_base => log_base
   )
+  only_if "test -d #{cstools_base}/config"
 end
 
 template "/etc/profile.d/mms-cstools.sh" do
@@ -113,14 +140,5 @@ end
 remote_file "#{cstools_base}/config/m2mr2-cs-spring.properties" do
   source "cs-tools/config/m2mr2-cs-spring.properties"
   mode 0644
+  only_if "test -d #{cstools_base}/config"
 end
-
-#link "#{cstools}/lib"  do
-#  to "#{deploy_dir}/mms-#{version}/cs-tools/lib"
-#  only_if "test -d #{deploy_dir}/mms-#{version}/cs-tools"
-#end
-
-#link "#{cstools}/archive"  do
-#  to "#{deploy_dir}/mms-#{version}/cs-tools/archive"
-#  only_if "test -d #{deploy_dir}/mms-#{version}/cs-tools"
-#end
