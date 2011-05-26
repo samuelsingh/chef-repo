dac36e9b097dfebb5bb637831d2b7cf8d14b85e6
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-05-26 14:29:39 +0000

    Updating stage-mms-vhosts role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2894 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/stage-mms-vhosts.rb
+++ roles/stage-mms-vhosts.rb
@@ -30,6 +30,14 @@ override_attributes(
       "holding_page" => "false",
       "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause.",
       "lb_alive_port" => 0
+    },
+    "localise.regression.mapofmedicine.com" => {
+      "srv_aliases" => [],
+      "deploy_dir" => "/var/shared/deployment/stage/root-mms",
+      "appserver" => "stage-clientmms-app-01.map-cloud-01.eu",
+      "holding_page" => "false",
+      "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause.",
+      "lb_alive_port" => 0
     }
   }
 )
