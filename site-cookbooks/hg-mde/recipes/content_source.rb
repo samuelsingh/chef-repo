85981020cd4f85cc2d532a51cb171a16721be32d
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-05-31 12:21:10 +0000

    Adding content_source recipe
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2899 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ site-cookbooks/hg-mde/recipes/content_source.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: hg-mde 
+# Recipe:: content_source
+#
+# This recipe should be installed on the node hosting the
+# relevant MD content packages
+
+group 'syncuser' do
+  action :create
+end
+
+user 'syncuser' do
+  username 'syncuser',
+  comment 'syncuser',
+  gid 'syncuser',
+  home '/home/syncuser',
+  supports :manage_home => true
+end
+
+directory '/home/syncuser' do
+  owner 'syncuser',
+  group 'syncuser',
+  action :create
+end
+
+remote_file '/home/syncuser/.ssh/authorized_keys' do
+  source 'authorized_keys',
+  owner 'syncuser',
+  group 'syncuser',
+  mode '0600'
+end
