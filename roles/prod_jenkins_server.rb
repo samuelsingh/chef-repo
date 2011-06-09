977fee102aa855c7b6974d356211e99cdb2a2c13
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-06-09 13:05:40 +0000

    Adding prod_jenkins_server.rb
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2927 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/prod_jenkins_server.rb
@@ -0,0 +1,11 @@
+name "prod_jenkins_server"
+description "Map of Medicine Hudson deployment server"
+
+run_list "role[tomcat]", "recipe[hudson_server]"
+
+override_attributes(
+  "tomcat" => {
+    "unpackwars" => "true",
+    "ajp_ports" => [9001]
+  }
+)
