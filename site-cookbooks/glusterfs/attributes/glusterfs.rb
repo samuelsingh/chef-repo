3cabbfac3c3fd5b27aa6b05e5d1a815d89629b2c
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-06-10 15:57:38 +0000

    Tweaking to be able to reduce memory usage for t1.micro
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2937 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/glusterfs/attributes/glusterfs.rb
+++ site-cookbooks/glusterfs/attributes/glusterfs.rb
@@ -4,6 +4,7 @@ end
 
 if recipe?("glusterfs::client")
   set_unless[:glusterfs][:client][:experimental] = "false"
+  set_unless[:glusterfs][:client][:iocache] = "256"
 end
 
   set_unless[:glusterfs][:mounts] = Array.new
