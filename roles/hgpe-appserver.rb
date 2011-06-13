64f9ff6f314bfc61a18a813e909ea5c3674cccda
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-06-13 15:19:22 +0000

    Adding jenkins_agent to hgpe role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2950 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/hgpe-appserver.rb
+++ roles/hgpe-appserver.rb
@@ -1,7 +1,7 @@
 name "hgpe-appserver"
 description "Configures HG / MDE application server"
 
-run_list "role[tomcat]", "recipe[hg-mde]"
+run_list "role[tomcat]", "recipe[hg-mde]", "recipe[jenkins_agent]"
 
 override_attributes(
   "tomcat" => {
@@ -10,5 +10,13 @@ override_attributes(
   "hg_mde" => {
     "hg_war_path" =>  "/var/shared/deployment/prod/healthguides/wars",
     "mde_war_path" =>  "/var/shared/deployment/prod/mde/wars"
+  },
+  "jenkins_agent" => {
+    "user" => "tomcat",
+    "group" => "tomcat",
+    "jvm" => {
+      "java_opts" => "-server -Xmx64m -Djava.awt.headless=true"
+    },
+    "server_url" => "http://jenkins-02.map-cloud-01.eu:8080/jenkins"
   }
 )
