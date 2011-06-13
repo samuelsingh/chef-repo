d83f7ee2a5fe9e8421872c8abd2cd9577a67dac4
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-06-13 11:45:04 +0000

    Making Hudson java opts configurable
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2940 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/hudson_server/attributes/default.rb
+++ site-cookbooks/hudson_server/attributes/default.rb
@@ -4,4 +4,5 @@ set[:hudson_server][:install_path]      = "/var/tomcat/server9001"
 set[:hudson_server][:war_path]          = "/var/hudson/deploy-wars"
 set[:hudson_server][:home]              = "/var/hudson"
 set[:hudson_server][:run_user]          = "tomcat"
+set_unless[:hudson_server][:java_opts] = "-server -Xmx768m -Djava.awt.headless=true"
 
