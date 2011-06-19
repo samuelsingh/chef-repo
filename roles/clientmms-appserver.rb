d060222c4d4d0a2040ea89629fe83bcc446d6284
andrewf <andrewf@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-06-19 19:06:01 +0000

    Updating Client MMS role
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@2969 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/clientmms-appserver.rb
+++ roles/clientmms-appserver.rb
@@ -1,37 +1,58 @@
 name "clientmms-appserver"
 description "Configures Client MMS application server"
 
-run_list "role[tomcat]", "recipe[mms]", "recipe[mms::cs-tools]", "recipe[mms::queue-manager]", "recipe[mms::helper-scripts]", "recipe[mms::cron]"
+#run_list "role[tomcat]", "recipe[mms]", "recipe[mms::cs-tools]", "recipe[mms::queue-manager]", "recipe[mms::helper-scripts]", "recipe[mms::cron]"
+
+run_list "role[tomcat]", "recipe[mms-2::application]", "recipe[mms-2::cs-tools]", "recipe[mms-2::cs-batch]", "role[prod-jenkins-agent]"
 
 override_attributes(
   "mms" => {
-    "fqdn" =>  "localise.mapofmedicine.com",
-    "deploy_dir" =>  "/var/shared/deployment/prod/client-mms",
-    "deployment_name" => "Client MMS",
-    "contentpath" => "/var/mms/content-out",
-    "content_in" => "/var/mms/content-in",
-    "dbhost" => "clientmms-db-01.map-cloud-01.eu",
-    "deployment" => {
-      "id" => "3",
-      "external_start" => "20000001",
-      "external_end" => "30000000"
+    "common"=> {
+      "interactive_usr"=> "sysadmin",
+      "fqdn"=> "localise.mapofmedicine.com",
+      "dbuser"=> "mtmuser",
+      "dbpass"=> "medic1",
+      "dbhost"=> "clientmms-db-01.map-cloud-01.eu",
+      "base"=> "/mnt/mms"
     },
-    "quartz" => {
-      "user" => "support@mapofmedicine.com",
-      "password" => "Ogohgha8"
-    },
-    "mapmanager" => {
-      "dbname" => "mapmanager_mcs"
-    },
-    "athens_link" => "true",
-    "multiple_views" => "true",
-    "datastore" => false,
-    "dbuser" => "mtmuser",
-    "dbpass" => "medic1"
+    "application"=> {
+      "deployment"=> {
+        "name" => "Client MMS",
+        "id" => "3",
+        "external_start" => "20000001",
+        "external_end" => "30000000"
+      },
+      "quartz"=> {
+        "user"=> "support@mapofmedicine.com",
+        "password"=> "Ogohgha8"
+      },
+      "repository"=> {
+        "dbname"=> "crx",
+        "datastore" => false
+      },
+      "mapmanager"=> {
+        "dbname"=> "mapmanager_mcs"
+      },
+      "mom"=> {
+        "dbname"=> "mappreview"
+      },
+      "live_md" => {
+        "name" => "Euro MD",
+        "url" => "http://app.mapofmedicine.com"
+      },
+      "athens_link" => "true",
+      "multiple_views" => "true"
+    }
   },
   "glusterfs" => {
     "client" => {
-      "experimental" => "true"
+      "stable" => "true"
     }
+  },
+  "tomcat" => {
+    "srv_dir" => "/var/tomcat",
+    "log_rotate_dir" => "/var/shared/rotated-logs",
+    "unpackwars" => "true"
   }
 )
+
