377b7eb604aff5dd3f406c0a8f14a62fba61e5fa
gerardo <gerardo@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-07-18 15:46:35 +0000

    restoring original entries
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3004 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/stage-md-vhosts.rb
+++ roles/stage-md-vhosts.rb
@@ -2,18 +2,10 @@ name "stage-md-vhosts"
 description "Configures apache vhosts for MD staging use"
 # List of recipes and roles to apply. Requires Chef 0.8, earlier versions use 'recipes()'.
 # run_list "recipe[zones]", "recipe[nfs_mounts]"
-run_list "recipe[map-display-3::vhost]", "recipe[lpa-vhost]", "recipe[stage-server]"
+run_list "recipe[map-display-vhost]", "recipe[lpa-vhost]", "recipe[stage-server]"
 
 override_attributes(
   "map_display_vhost" => {
-    "stage-mdprov.map-cloud-01.eu" => {
-      "srv_aliases" => ['euromd.regression.mapofmedicine.com','euromd.beta.mapofmedicine.com'],
-      "deploy_dir" => "/var/shared/deployment/stage/euro-md",
-      "holding_page" => "false",
-      "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause.",
-      "appserver" => "stage-euromd-app-01.map-cloud-01.eu",
-      "lb_alive_port" => 0
-    },
     "euromd.contentuat.mapofmedicine.com" => {
       "srv_aliases" => [],
       "deploy_dir" => "/var/shared/deployment/stage/euro-md",
@@ -76,3 +68,5 @@ override_attributes(
     }
   }
 )
+
+
