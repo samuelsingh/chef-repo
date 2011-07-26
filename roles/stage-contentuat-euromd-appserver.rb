57f562f3bb6d449e11321d608ab0aea8e1e98a22
gerardo <gerardo@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-07-26 22:25:47 +0000

    test role for euromd
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3011 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/stage-contentuat-euromd-appserver.rb
+++ roles/stage-contentuat-euromd-appserver.rb
@@ -1,40 +1,16 @@
-name "stage-contentuat-euromd-appserver"
-description "Configures Stage Content MD application server"
+name "stage euromd-md-appserver"
+description "Configures CEP ContentUAT MD application server on the stage euromd server"
 
-run_list "recipe[java]", "recipe[tomcat]", "role[web-server]", "recipe[map-display-3::application]", "recipe[map-display-3::contentloader]", "recipe[map-display-3::vhost]"
+run_list "recipe[java]", "recipe[tomcat]", "recipe[map-display]", "recipe[map-display::contentloader]"
 
 override_attributes(
   "map_display" => {
-    "common" => {
-      "dbhost" => "stage-db-02.map-cloud-01.eu",
-      "dbname" => "uat_mtmdb",
-      "dbuser" => "mtmuser",
-      "dbpass" => "medic1"
-    },
-    "application" => {
-      "md_fqdn" =>  "euromd.contentuat.mapofmedicine.com",
-      "geoip_server" => "geoip.mapofmedicine.com",
-      "save_ram" => "true"
-    },
-    "contentloader" => {
-      "path" => "/usr/local/contentloader",
-      "dist_path" => "/usr/local/contentloader-dist"
-    },
-    "vhost" => {
-      "euromd.contentuat.mapofmedicine.com" => {
-        "srv_aliases" => [],
-        "holding_page" => "false",
-        "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause.",
-        "appserver" => "127.0.0.1",
-        "lb_alive_port" => 0
-      }
-    },
-  "tomcat" => {
-    "srv_dir" => "/var/tomcat",
-    "log_rotate_dir" => "/var/shared/rotated-logs",
-    "unpackwars" => "true"
+    "md_fqdn" =>  "integration.mapofmedicine.com",
+    "deploy_dir" =>  "/var/shared/deployment/euromd",
+    "dbhost" => "stage-db-02.map-cloud-01.eu",
+    "dbname" => "uat_mtmdb",
+    "dbuser" => "mtmuser",
+    "dbpass" => "medic1"
   }
-}
 )
-
 
