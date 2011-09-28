dd201914b37a219ba9f5a24890e03053f06da778
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-09-28 17:27:29 +0000

    update integration-md-appserver.rb
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3068 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/integration-md-appserver.rb
+++ roles/integration-md-appserver.rb
@@ -1,16 +1,29 @@
 name "integration-md-appserver"
-description "Configures Production MD application server on the integration server"
+description "Configures  Integration application server"
 
-run_list "recipe[java]", "recipe[tomcat]", "recipe[map-display]", "recipe[map-display::contentloader]"
+run_list "recipe[java]", "role[tomcat]", "recipe[map-display-3::application]", "recipe[map-display-3::contentloader]", "recipe[map-display-3::rotate_logs]"
 
 override_attributes(
   "map_display" => {
-    "md_fqdn" =>  "integration.mapofmedicine.com",
-    "deploy_dir" =>  "/var/shared/deployment/prod/integration-md",
-    "dbhost" => "md-db-01.map-cloud-01.eu",
-    "dbname" => "integration_mtmdb",
-    "dbuser" => "mtmuser",
-    "dbpass" => "medic1"
+    "common" => {
+      "dbhost" => "md-db-01.map-cloud-01.eu",
+      "dbname" => "integration_mtmdb",
+      "dbuser" => "mtmuser",
+      "dbpass" => "medic1"
+    },
+    "application" => {
+      "md_fqdn" =>  "integration.mapofmedicine.com",
+      "geoip_server" => "geoip.mapofmedicine.com",
+      "save_ram" => "true"
+    },
+    "contentloader" => {
+      "path" => "/usr/local/contentloader",
+      "dist_path" => "/usr/local/contentloader-dist"
+    }
+  },
+  "tomcat" => {
+    "srv_dir" => "/var/tomcat",
+    "log_rotate_dir" => "/var/shared/rotated-logs",
+    "unpackwars" => "true"
   }
 )
-
