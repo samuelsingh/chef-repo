ec3c15cba42efc202ea8afba147a83538d858534
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-10-21 15:33:58 +0000

    update contentuat-euromd-appserver.rb
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3087 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/contentuat-euromd-appserver.rb
+++ roles/contentuat-euromd-appserver.rb
@@ -1,7 +1,7 @@
 name "contentuat-euromd-appserver"
 description "Configures application server for content uat"
 
-run_list "recipe[java]", "role[tomcat]", "recipe[map-display-3::application]", "recipe[map-display-3::contentloader]", "recipe[map-display-3::rotate_logs]"
+run_list "recipe[java]", "role[tomcat]", "recipe[map-display-3::contentloader]", "recipe[map-display-3::rotate_logs]"
 
 override_attributes(
   "map_display" => {
@@ -10,11 +10,6 @@ override_attributes(
       "dbname" => "uat_mtmdb",
       "dbuser" => "mtmuser",
       "dbpass" => "medic1"
-    },
-"application" => {
-      "md_fqdn" =>  "euromd.contentuat.mapofmedicine.com",
-      "geoip_server" => "geoip.mapofmedicine.com",
-      "save_ram" => "true"
     },
     "contentloader" => {
       "path" => "/usr/local/contentloader",
