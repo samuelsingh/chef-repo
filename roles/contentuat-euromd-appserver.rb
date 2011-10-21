b1c9d633d34dcd1386d6ab16db28e23235be2c0e
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-10-21 16:25:49 +0000

    update contentuat-euromd-appserver.rb
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3088 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/contentuat-euromd-appserver.rb
+++ roles/contentuat-euromd-appserver.rb
@@ -1,24 +1,24 @@
 name "contentuat-euromd-appserver"
 description "Configures application server for content uat"
 
-run_list "recipe[java]", "role[tomcat]", "recipe[map-display-3::contentloader]", "recipe[map-display-3::rotate_logs]"
+run_list "recipe[java]", "recipe[tomcat]", "recipe[tomcat::rotate_logs]", "recipe[map-display]", "recipe[map-display::contentloader]"
 
 override_attributes(
   "map_display" => {
-    "common" => {
-      "dbhost" => "stage-db-02.map-cloud-01.eu",
-      "dbname" => "uat_mtmdb",
-      "dbuser" => "mtmuser",
-      "dbpass" => "medic1"
-    },
-    "contentloader" => {
-      "path" => "/usr/local/contentloader",
-      "dist_path" => "/usr/local/contentloader-dist"
+    "md_fqdn" =>  "euromd.contentuat.mapofmedicine.com",
+    "deploy_dir" =>  "/var/shared/deployment/stage/euro-md",
+    "dbhost" => "stage-db-02.map-cloud-01.eu",
+    "dbname" => "uat_mtmdb",
+    "dbuser" => "mtmuser",
+    "dbpass" => "medic1"
+  },
+  "glusterfs" => {
+    "client" => {
+      "experimental" => "true"
     }
   },
   "tomcat" => {
     "srv_dir" => "/var/tomcat",
-    "log_rotate_dir" => "/var/shared/rotated-logs",
-    "unpackwars" => "true"
+    "log_rotate_dir" => "/var/shared/rotated-logs"
   }
 )
