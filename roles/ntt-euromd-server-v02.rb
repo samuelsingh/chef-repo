b72c1e16220a53bec5b63ca4725fb18b9a80c4de
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2011-11-07 15:25:00 +0000

    add new role ntt-euromd-server-v02.rb
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3098 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/ntt-euromd-server-v02.rb
@@ -0,0 +1,32 @@
+name "ntt-euromd-server-v01"
+description "Configures Euro MD application server"
+
+run_list "role[web-server]", "recipe[map-display-3::vhost_v01]", "recipe[map-display-3::lpa_vhost]", "recipe[map-display-3::time_transactions]"
+
+override_attributes(
+  "map_display" => {
+    "vhost_v01" => {
+      "app.mapofmedicine.com" => {
+        "srv_aliases" => ["app.mapofmedicine.info"],
+        "holding_page" => "false",
+        "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause.",
+        "appserver" => "euromd-app-02.map-cloud-01.eu",
+        "lb_alive_port" => 0
+      }
+    },
+    "time_transactions" => {
+      "base" => "/usr/local/webinject",
+      "user" => "webinject",
+      "group" => "webinject",
+      "out_dir" => "/var/shared/webinject"
+    },
+    "lpa_vhost" => {
+      "localcare.mapofmedicine.com" => {
+        "srv_aliases" => [],
+        "holding_page" => "false",
+        "appserver" => "127.0.0.1",
+        "lb_alive_port" => 0
+      }
+    }
+  }
+)
