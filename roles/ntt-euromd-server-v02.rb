9f97c7d3026a94d482aaad214c0c87b6ffa36938
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2012-06-26 14:05:37 +0000

    ntt-euromd-server-v01
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3208 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/ntt-euromd-server-v02.rb
+++ roles/ntt-euromd-server-v02.rb
@@ -1,16 +1,16 @@
 name "ntt-euromd-server-v02"
 description "Configures euromd-app-02"
 
-run_list "role[web-server]", "recipe[map-display-3::vhost]", "recipe[map-display-3::lpa_vhost]", "recipe[map-display-3::time_transactions]"
+run_list "role[web-server]", "recipe[map-display-3::vhost_v01]", "recipe[map-display-3::lpa_vhost]", "recipe[map-display-3::time_transactions]"
 
 override_attributes(
   "map_display" => {
-    "vhost" => {
-      "app.mapofmedicine.com" => {
+    "vhost_v01" => {
+      "test.app.mapofmedicine.com" => {
         "srv_aliases" => ["app.mapofmedicine.info"],
         "holding_page" => "false",
         "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause.",
-        "appserver" => "euromd-app-02.map-cloud-01.eu",
+        "appserver" => "md-app-02.map-cloud-01.eu",
         "lb_alive_port" => 8050
       }
     },
@@ -21,7 +21,7 @@ override_attributes(
       "out_dir" => "/var/shared/webinject"
     },
     "lpa_vhost" => {
-      "localcare.mapofmedicine.com" => {
+      "test.localcare.mapofmedicine.com" => {
         "srv_aliases" => [],
         "holding_page" => "false",
         "appserver" => "127.0.0.1",
