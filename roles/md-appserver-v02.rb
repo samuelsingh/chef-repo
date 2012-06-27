083c13f63bcb570d8cbd9c6221aebae834a91907
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2012-06-27 12:50:03 +0000

    md-appserver-v02
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3212 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/md-appserver-v02.rb
@@ -0,0 +1,28 @@
+name "md-appserver-v02"
+description "Configure MD app server"
+run_list "role[tomcat]", "recipe[map-display-3::application]", "recipe[map-display-3::contentloader]", "recipe[map-display-3::rotate_logs]"
+
+override_attributes(
+  "map_display" => {
+    "common" => {
+      "dbhost" => "md-db-01.map-cloud-01.eu",
+      "dbname" => "live_mtmdb",
+      "dbuser" => "mtmuser",
+      "dbpass" => "medic1"
+    },
+    "application" => {
+      "md_fqdn" =>  "app.mapofmedicine.com",
+      "geoip_server" => "geoip.mapofmedicine.com",
+      "save_ram" => "true"
+    },
+    "contentloader" => {
+      "path" => "/usr/local/contentloader",
+      "dist_path" => "/usr/local/contentloader-dist"
+    }
+  },
+  "tomcat" => {
+    "srv_dir" => "/var/tomcat",
+    "log_rotate_dir" => "/var/shared/rotated-logs",
+    "unpackwars" => "true"
+  }
+)
