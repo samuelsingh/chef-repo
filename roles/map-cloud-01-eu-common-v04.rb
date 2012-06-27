8b91317e44773d1e59074b5b7f3c2949d3dbe591
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2012-06-27 13:55:31 +0000

    changes on ryslog
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3215 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/map-cloud-01-eu-common-v04.rb
+++ roles/map-cloud-01-eu-common-v04.rb
@@ -1,6 +1,6 @@
 name "map-cloud-01-eu-common-v04"
 description "Configuration in md-app servers"
-run_list "recipe[zones]", "recipe[standard_users]", "recipe[sudo]", "recipe[openssh-server-ec2]", "recipe[exim::satellite]", "recipe[mom-scripts]", "recipe[ntp]", "recipe[snmpd]", "recipe[zenoss::client]", "role[rsyslog-mom-client]"
+run_list "recipe[zones]", "recipe[standard_users]", "recipe[sudo]", "recipe[openssh-server-ec2]", "recipe[exim::satellite]", "recipe[mom-scripts]", "recipe[ntp]", "recipe[snmpd]", "recipe[zenoss::client]", "recipe[rsyslog_mom]"
 
 override_attributes(
   "zones" => {
