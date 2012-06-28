b192edd2e7e7d37fccc51db8d7b7a6c881c8afd0
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2012-06-28 13:11:12 +0000

    ntt-euromd-server-v02
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3225 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/ntt-euromd-server-v02.rb
+++ roles/ntt-euromd-server-v02.rb
@@ -1,7 +1,7 @@
 name "ntt-euromd-server-v02"
 description "Configures euromd-app-02"
 
-run_list "recipe[map-display-3::lpa_vhost]", "recipe[map-display-3::time_transactions]"
+run_list "role[web-server]", "recipe[map-display-3::vhost_v01]", "recipe[map-display-3::lpa_vhost]", "recipe[map-display-3::time_transactions]"
 
 override_attributes(
   "map_display" => {
