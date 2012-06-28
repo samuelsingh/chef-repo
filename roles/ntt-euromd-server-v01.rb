56a4dd79840ab405100dbc5d4dba242db71730d0
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2012-06-28 11:53:36 +0000

    ntt-euromd-server-v01
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3221 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/ntt-euromd-server-v01.rb
+++ roles/ntt-euromd-server-v01.rb
@@ -4,6 +4,13 @@ description "Configures Euro MD web-application server"
 run_list "role[web-server]", "recipe[map-display-3::vhost_v01]", "recipe[map-display-3::lpa_vhost]", "recipe[map-display-3::time_transactions]"
 
 override_attributes(
+"apache" => {
+    "listen_ports" => [
+     "80",
+     "8051",
+   "443"
+    ]
+},
   "map_display" => {
     "vhost_v01" => {
       "app.mapofmedicine.com" => {
@@ -11,7 +18,7 @@ override_attributes(
         "holding_page" => "false",
         "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause.",
         "appserver" => "md-app-01.map-cloud-01.eu",
-        "lb_alive_port" => 8050
+        "lb_alive_port" => 0
       }
     },
     "time_transactions" => {
