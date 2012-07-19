9ab1f12f464ba7daf20b57880576213797775aaa
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2012-07-19 16:30:25 +0000

    map-cloud-01-eu-common
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3247 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/map-cloud-01-eu-common-v05.rb
@@ -0,0 +1,18 @@
+name "map-cloud-01-eu-common-v05"
+description "Configuration in md-app servers"
+run_list "recipe[standard_users]", "recipe[sudo]", "recipe[openssh-server-ec2]", "recipe[exim::satellite]", "recipe[mom-scripts]", "recipe[ntp]", "recipe[snmpd]", "recipe[zenoss::client]" 
+
+override_attributes(
+  "zones" => {
+    "name" =>  "euaws"
+  },
+  "snmpd" => {
+    "community" => "ufahji3K",
+    "syslocation" => "map-cloud-01.eu, amazon eu-west-1",
+    "syscontact" => "systems@mapofmedicine.com"
+  },
+  "ec2" => {
+    "ec2_access_key" => "",
+    "ec2_secret_key" => ""
+  },
+)
