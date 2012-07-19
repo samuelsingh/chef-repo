5991261741db2a4b95e3c673da49f1df7ee246b8
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2012-07-19 14:54:13 +0000

    intergration app
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3246 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/integration-pre-prod-app.rb
@@ -0,0 +1,29 @@
+name "integration-pre-prod-app"
+description "Configures  Integration application server"
+
+run_list "role[tomcat]", "recipe[map-display-3::application]", "recipe[map-display-3::contentloader]", "recipe[map-display-3::rotate_logs]"
+
+override_attributes(
+  "map_display" => {
+    "common" => {
+      "dbhost" => "integration-db-01.map-cloud-01.eu",
+      "dbname" => "integration_mtmdb01",
+      "dbuser" => "mtmuser",
+      "dbpass" => "medic1"
+    },
+    "application" => {
+      "md_fqdn" =>  "integration.mapofmedicine.com",
+      "geoip_server" => "geoip.mapofmedicine.com",
+      "save_ram" => "true"
+    },
+    "contentloader" => {
+      "path" => "/usr/local/contentloader",
+      "dist_path" => "/usr/local/contentloader-dist"
+    }
+  },
+  "tomcat" => {
+    "srv_dir" => "/var/tomcat",
+    "log_rotate_dir" => "/var/shared/rotated-logs",
+    "unpackwars" => "true"
+  }
+)
