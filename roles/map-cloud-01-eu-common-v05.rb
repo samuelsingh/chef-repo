8923eb2463258f82f88b6f7936a74131cd63ebd2
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2012-07-30 14:38:50 +0000

    map-cloud-01-eu-common
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3255 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/map-cloud-01-eu-common-v05.rb
+++ roles/map-cloud-01-eu-common-v05.rb
@@ -1,6 +1,6 @@
 name "map-cloud-01-eu-common-v05"
-description "Configuration in md-app servers"
-run_list "recipe[standard_users]", "recipe[sudo]" 
+description "Configuration common to all machines in map-cloud-01.eu. V0.5"
+run_list "recipe[zones]", "recipe[glusterfs::client]", "recipe[standard_users]", "recipe[sudo]", "recipe[openssh-server-ec2]", "recipe[exim::satellite]", "recipe[mom-scripts]", "recipe[ntp]", "recipe[snmpd]", "recipe[zenoss::client]", "role[rsyslog-mom-client]"
 
 override_attributes(
   "zones" => {
@@ -14,5 +14,11 @@ override_attributes(
   "ec2" => {
     "ec2_access_key" => "",
     "ec2_secret_key" => ""
+  },
+  "glusterfs" => {
+    "client" => {
+      "stable" => "true",
+      "mounts" => ["shared,/var/shared","eph_share,/var/eph_share"]
+    }
   }
 )
