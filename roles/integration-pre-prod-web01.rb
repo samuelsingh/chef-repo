57263b5360486ca4c2b00843f200f5a2c9c81d2a
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2012-08-20 14:19:46 +0000

    update for md3
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3259 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/integration-pre-prod-web01.rb
+++ roles/integration-pre-prod-web01.rb
@@ -1,7 +1,7 @@
 name "integration-pre-prod-web01"
 description "Configure preprod Web-01 Server"
 
-run_list "role[web-server]", "recipe[map-display-3::vhost_integration]"
+run_list "role[web-server]", "recipe[map-display-3::vhost_integration]", "recipe[map-display-3::lpa_vhost]", "recipe[map-display-3::time_transactions]"
 
 override_attributes(
 "apache" => {
@@ -13,12 +13,26 @@ override_attributes(
 },
   "map_display" => {
     "vhost_integration" => {
-      "preprod.mapofmedicine.com" => {
-        "srv_aliases" => ["preprod.mapofmedicine.info"],
+      "app.mapofmedicine.com" => {
+        "srv_aliases" => ["app.mapofmedicine.info"],
         "holding_page" => "false",
         "holding_page_msg" => "This Map of Medicine service is offline for emergency maintenance.  We apologise for any inconvenience this may cause.",
-        "appserver" => "preprod-app-01.map-cloud-01.eu",
+        "appserver" => "md3-app-01.map-cloud-01.eu",
 	"lb_alive_port" => 8050
+      }
+    },
+    "time_transactions" => {
+      "base" => "/usr/local/webinject",
+      "user" => "webinject",
+      "group" => "webinject",
+      "out_dir" => "/var/shared/webinject"
+    },
+    "lpa_vhost" => {
+      "localcare.mapofmedicine.com" => {
+        "srv_aliases" => [],
+        "holding_page" => "false",
+        "appserver" => "127.0.0.1",
+        "lb_alive_port" => 0
       }
     }
   }
