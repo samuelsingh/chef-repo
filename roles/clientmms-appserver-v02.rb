136a656653c14e87dae6846eb5d7551063d314dc
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2012-08-29 17:07:40 +0000

    clientmms conf
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3273 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/clientmms-appserver-v02.rb
+++ roles/clientmms-appserver-v02.rb
@@ -3,7 +3,7 @@ description "Configures Client MMS application server"
 
 #run_list "role[tomcat]", "recipe[mms]", "recipe[mms::cs-tools]", "recipe[mms::queue-manager]", "recipe[mms::helper-scripts]", "recipe[mms::cron]"
 
-run_list "recipe[mms-2::cs-tools]", "recipe[mms-2::cs-batch]", "recipe[mms-2::cron]", role[tomcat]", "recipe[mms-2::application]"
+run_list "recipe[mms-2::cs-tools]", "recipe[mms-2::cs-batch]", "recipe[mms-2::cron]", role[tomcat]"
 
 override_attributes(
   "mms" => {
