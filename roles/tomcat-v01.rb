856dbbd87eb637615cc1339035ad946cd82b40b7
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2012-08-30 09:37:36 +0000

    tomcat version 01
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3278 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/tomcat-v01.rb
@@ -0,0 +1,11 @@
+name "tomcat"
+description "Configures Apache Tomcat 6 application server"
+
+run_list "recipe[tomcat]", "recipe[tomcat::rotate_logs]" 
+
+override_attributes(
+  "tomcat" => {
+    "srv_dir" => "/var/tomcat",
+    "log_rotate_dir" => "/var/shared/rotated-logs"
+  }
+)
