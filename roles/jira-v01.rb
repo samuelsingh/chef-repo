73566f0f6218b0269befa4ae6f41fdd68f5d4248
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2012-11-28 15:44:59 +0000

    add wikijira recipe
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3322 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/jira-v01.rb
@@ -0,0 +1,16 @@
+name "jira-v01"
+description "Configures jira"
+
+run_list "recipe[jira_mom::jira-v01]"
+
+override_attributes(
+  "jira" => {
+    "run_user" => "tomcat",
+    "database_host" => "jiradb",
+    "version" => "atlassian-jira-4.1.2",
+    "database" => "mysql",
+    "database_user" => "jira_user",
+    "install_path" => "/var/tomcat/server9002",
+    "database_password" => "medic1"
+  }
+)
