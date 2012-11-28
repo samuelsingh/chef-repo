6f5f23be07d988ff0d6271bc1fec62e07075e6d8
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2012-11-28 15:04:06 +0000

    add wikijira recipe
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3315 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/wikijira-appserver-v01.rb
+++ roles/wikijira-appserver-v01.rb
@@ -1,7 +1,7 @@
 name "wikijira-appserver-v01"
 description "Configures Jira/Wiki application server"
 
-run_list "recipe[tomcat::rotate_logs]", "role[confluence::confluence-v01.rb]", "role[jira::jira-v01.rb]"
+run_list "recipe[tomcat::rotate_logs]", "role[confluence::confluence-v01]", "role[jira::jira-v01]"
 
 override_attributes(
   "tomcat" => {
