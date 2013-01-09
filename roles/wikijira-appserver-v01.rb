4cab0367bda65191f6a0b6a7524ffd2447db7ae6
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2013-01-09 17:06:09 +0000

    changed on wikijira
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3326 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/wikijira-appserver-v01.rb
+++ roles/wikijira-appserver-v01.rb
@@ -1,7 +1,7 @@
 name "wikijira-appserver-v01"
 description "Configures Jira/Wiki application server"
 
-run_list "recipe[tomcat::rotate_logs]", "role[confluence-v01]", "role[jira-v01]"
+run_list "recipe[tomcat::rotate_logs]", "role[jira-v01]"
 
 override_attributes(
   "tomcat" => {
