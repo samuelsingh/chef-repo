7bfb77f05902482442e0d99aa464824624790956
nitins <nitins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2013-10-11 09:39:15 +0000

    update trainingmms recipe
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3357 daf2e314-7611-4b85-95e2-b5f857fec4d4

---
+++ roles/trainingmms-appserver-v01.rb
@@ -0,0 +1,57 @@
+name "trainingmms-appserver"
+description "Configures Training MMS application server"
+
+#run_list "role[tomcat]", "recipe[mms]", "recipe[mms::cs-tools]", "recipe[mms::queue-manager]", "recipe[mms::helper-scripts]", "recipe[mms::cron]"
+
+run_list "role[tomcat-v01]", "recipe[mms-2::application]", "recipe[mms-2::cs-tools]", "recipe[mms-2::cs-batch]", "role[prod-jenkins-agent]"
+
+override_attributes(
+  "mms" => {
+    "common"=> {
+      "interactive_usr"=> "sysadmin",
+      "fqdn"=> "training-mms.mapofmedicine.com",
+      "dbuser"=> "mtmuser",
+      "dbpass"=> "MtMUs3r",
+      "dbhost"=> "preprod-db-01.map-cloud-01.eu",
+      "base"=> "/mnt/mms"
+    },
+    "application"=> {
+      "deployment"=> {
+        "name" => "Training MMS",
+        "id" => "50",
+        "external_start" => "30000001",
+        "external_end" => "40000000"
+      },
+      "quartz"=> {
+        "user"=> "quartz@mapofmedicine.com",
+        "password"=> "SoZ8siMi"
+      },
+      "repository"=> {
+        "dbname"=> "training_repo"
+      },
+      "mapmanager"=> {
+        "dbname"=> "training_mcs"
+      },
+      "mom"=> {
+        "dbname"=> "training_preview"
+      },
+      "live_md" => {
+        "name" => "Euro MD",
+        "url" => "http://app.mapofmedicine.com"
+      },
+      "athens_link" => "true",
+      "multiple_views" => "true"
+    }
+  },
+  "glusterfs" => {
+    "client" => {
+      "stable" => "true"
+    }
+  },
+  "tomcat" => {
+    "srv_dir" => "/var/tomcat",
+    "log_rotate_dir" => "/var/shared/rotated-logs",
+    "unpackwars" => "true"
+  }
+)
+
