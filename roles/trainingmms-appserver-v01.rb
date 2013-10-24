16e6840613a327bfb1ede1d873b83901dd8a33dc
sachins <sachins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2013-10-24 10:50:44 +0000

    prepro
    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3363 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- roles/trainingmms-appserver-v01.rb
+++ roles/trainingmms-appserver-v01.rb
@@ -12,7 +12,7 @@ override_attributes(
       "fqdn"=> "training-mms.mapofmedicine.com",
       "dbuser"=> "mtmuser",
       "dbpass"=> "MtMUs3r",
-      "dbhost"=> "preprod-db-01.map-cloud-01.eu",
+      "dbhost"=> "sspreprod-db-01.map-cloud-01.eu",
       "base"=> "/mnt/mms",
 	  "mms","application","me_url" => "http://training-mms.mapofmedicine.com/mapmanager/mapeditor/"
 	  
