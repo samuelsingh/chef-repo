2677af5969ba90f76799008598ce3ce35c31d6ce
sachins <sachins@daf2e314-7611-4b85-95e2-b5f857fec4d4>
2014-01-14 15:59:53 +0000

    git-svn-id: http://svn.mapofmedicine.com/map-sys/branches/CHEF_EXPERIMENTAL/chef-repo@3427 daf2e314-7611-4b85-95e2-b5f857fec4d4

--- site-cookbooks/mms-2-SS/recipes/application.rb
+++ site-cookbooks/mms-2-SS/recipes/application.rb
@@ -4,3 +4,19 @@ directory "/var/tmp/samuelsingh" do
   mode 00644
   action :create
 end
+
+
+directory "/var/tmp/James" do
+  owner "root"
+  group "sysadmin"
+  mode 00644
+  action :create
+end
+
+
+directory "/var/tmp/ss" do
+  owner "root"
+  group "root"
+  mode 00644
+  action :create
+end
